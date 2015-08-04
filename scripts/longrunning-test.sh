385cefc9b6870ea2344d350157b66be7a0eb27bc
Micah Young <myoung@pivotal.io>
2015-08-04 16:59:54 -0400

    Uses cf-linux from nora/bin
    Signed-off-by: John_Shahid <jshahid@pivotal.io>

--- scripts/longrunning-test.sh
+++ scripts/longrunning-test.sh
@@ -4,19 +4,21 @@ set -ex
 
 # Assumes diego-windows-msi under ./diego-windows-msi
 
+nora_dir=diego-windows-msi/src/github.com/pivotal-cf-experimental/nora
 appname=nora
 domain=greenhouse-development2.cf-app.com
 url=$appname.$domain
 
-cf api api.$domain
-cf login -u $CF_USERNAME -p $CF_PASSWORD
+cf="${nora_dir}/bin/cf-linux"
+$cf api api.$domain
+$cf login -u $CF_USERNAME -p $CF_PASSWORD
 
-pushd diego-windows-msi/src/github.com/pivotal-cf-experimental/nora/assets/nora
+pushd ${nora_dir}/assets/nora
   ./make_a_nora $appname
 popd
 
-cf scale -i 3 $appname
+$cf scale -i 3 $appname
 for i in {1..10}; do
     curl $url
 done
-cf d $appname
+$cf d $appname
