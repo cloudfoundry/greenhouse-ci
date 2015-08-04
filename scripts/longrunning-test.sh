ba8ea3d2df2cd9867ea800ec41ffdf33b903336e
Micah Young <myoung@pivotal.io>
2015-08-04 17:43:01 -0400

    fixes few issues with the longrunning test script
    Signed-off-by: John_Shahid <jshahid@pivotal.io>

--- scripts/longrunning-test.sh
+++ scripts/longrunning-test.sh
@@ -4,21 +4,43 @@ set -ex
 
 # Assumes diego-windows-msi under ./diego-windows-msi
 
-nora_dir=diego-windows-msi/src/github.com/pivotal-cf-experimental/nora
 appname=nora
 domain=greenhouse-development2.cf-app.com
 url=$appname.$domain
 
-cf="${nora_dir}/bin/cf-linux"
-$cf api api.$domain
-$cf login -u $CF_USERNAME -p $CF_PASSWORD
+export nora_dir=$PWD/diego-windows-msi/src/github.com/pivotal-cf-experimental/nora
+function cf {
+    ${nora_dir}/bin/cf-linux "$@"
+}
+
+export -f cf
+
+cf api --skip-ssl-validation api.$domain
+cf login -u $CF_USERNAME -p $CF_PASSWORD -o ORG -s SPACE
 
 pushd ${nora_dir}/assets/nora
   ./make_a_nora $appname
 popd
 
-$cf scale -i 3 $appname
+trap "cf d $appname -f" EXIT
+
+cf scale -i 3 $appname
+
+for i in {1..300}; do
+    count=`cf app $appname | grep running | wc -l`
+    if [ $count -eq 3 ]; then
+        break;
+    fi
+    sleep 1
+done
+
+if [ $count -ne 3 ]; then
+    echo "scaling failed"
+    exit 1
+fi
+
 for i in {1..10}; do
     curl $url
 done
-$cf d $appname
+
+cf d $appname -f
