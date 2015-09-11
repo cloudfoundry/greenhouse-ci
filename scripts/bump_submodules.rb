730caf4945a654c86a53626f441cd404d3dfa3f0
Micah Young <myoung@pivotal.io>
2015-09-11 17:21:49 -0400

    Add task and script for automatic diego-windows-msi bump
    [#103225364]
    
    Signed-off-by: Ben_Moss <bmoss@pivotal.io>

---
+++ scripts/bump_submodules.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+def sh(cmd)
+  puts cmd
+  system(cmd) || exit(1)
+end
+
+modules = Dir.chdir("diego-release") do
+  `git submodule status --recursive`
+end
+
+Dir.chdir("diego-windows-msi") do
+  modules.split(/\n/).each do |line|
+    a = line.split(/\s+/)
+    sha = a[1]
+    dir = a[2]
+    puts dir
+
+    if Dir.exists?(dir)
+      puts "\t#{sha}"
+      Dir.chdir(dir) do
+        `git fetch && git co #{sha}`
+      end
+    end
+  end
+
+  puts "----- Set git identity"
+  sh 'git config user.email "cf-netgarden-eng@pivotal.io"'
+  sh 'git config user.name "CI (Automated)"'
+
+  puts "----- Update master branch on origin"
+  sh 'git add -A'
+  sh %{git diff --cached --submodule | ruby -e "puts 'Bump submodules'; puts; puts STDIN.read" | git commit --file -}
+
+  puts "----- DEBUG: show the commit we just created"
+  sh 'git --no-pager show HEAD'
+end
