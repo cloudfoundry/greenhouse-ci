a691143ad8dece8a4e3303deaea09926c5cdf4eb
Micah Young <myoung@pivotal.io>
2015-09-11 18:02:16 -0400

    Don't error out if there's no submodules to bump
    Signed-off-by: Ben_Moss <bmoss@pivotal.io>

--- scripts/bump_submodules.rb
+++ scripts/bump_submodules.rb
@@ -5,6 +5,10 @@ def sh(cmd)
   system(cmd) || exit(1)
 end
 
+def submodules_changed?
+  !system("git diff --exit-code")
+end
+
 modules = Dir.chdir("diego-release") do
   `git submodule status --recursive`
 end
@@ -29,9 +33,12 @@ Dir.chdir("diego-windows-msi") do
   sh 'git config user.name "CI (Automated)"'
 
   puts "----- Update master branch on origin"
-  sh 'git add -A'
-  sh %{git diff --cached --submodule | ruby -e "puts 'Bump submodules'; puts; puts STDIN.read" | git commit --file -}
-
-  puts "----- DEBUG: show the commit we just created"
-  sh 'git --no-pager show HEAD'
+  if submodules_changed?
+    sh 'git add -A'
+    sh %{git diff --cached --submodule | ruby -e "puts 'Bump submodules'; puts; puts STDIN.read" | git commit --file -}
+    puts "----- DEBUG: show the commit we just created"
+    sh 'git --no-pager show HEAD'
+  else
+    puts "----- Nothing to commit"
+  end
 end
