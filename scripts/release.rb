9ee2a65dcb97df39e0b03e1bd697a8843e3a0989
John Shahid <jshahid@pivotal.io>
2015-09-28 16:40:43 -0400

    updates the release script to create a release in both repos
    [#102200152]
    
    Signed-off-by: Matthew Horan <mhoran@pivotal.io>

--- scripts/release.rb
+++ scripts/release.rb
@@ -20,23 +20,23 @@ def generate_file
   Dir::glob('./greenhouse-install-script-generator/generate*.exe').first
 end
 
-def msi_file
-  Dir::glob('./msi-file/DiegoWindowsMSI*.msi').first
+def diego_windows_msi_file
+  Dir::glob('./diego-windows-msi-file/DiegoWindows*.msi').first
+end
+
+def garden_windows_msi_file
+  Dir::glob('./garden-windows-msi-file/GardenWindows*.msi').first
 end
 
 def github
   @github ||= Octokit::Client.new access_token: token
 end
 
-def repo
-  'cloudfoundry-incubator/diego-windows-msi'
-end
-
-def create_github_tag
-  msi_sha = revision("diego-windows-msi")
+def create_github_tag(repo, version)
+  msi_sha = revision(File.basename repo)
   puts "Creating release #{release} with sha #{msi_sha}"
   github.create_tag repo,
-                    release,
+                    version,
                     "Release #{release}",
                     msi_sha,
                     "commit",
@@ -45,12 +45,12 @@ def create_github_tag
                     Time.now.utc.iso8601
 
   github.create_release(repo,
-                        release,
-                        name: release,
-                        body: cf_diego_release_text(release))
+                        version,
+                        name: version,
+                        body: cf_diego_release_text(version))
 end
 
-def cf_diego_release_text(release)
+def cf_diego_release_text
   diego_release = revision("diego-release") rescue "UNKNOWN"
   cf_release = revision("cf-release") rescue "UNKNOWN"
   release_body = <<-BODY
@@ -60,36 +60,31 @@ BODY
   release_body
 end
 
-def content_type filename
-  if File.extname(filename) == ".md"
-    "text/plain"
-  else
-    "application/octet-stream"
-  end
-end
-
 def upload_release_assets(filepath, release, filename=nil)
   filename ||= File.basename filepath
   github.upload_asset release[:url],
                       filepath,
-                      content_type: content_type(filename),
                       name: filename
 end
 
-def get_release_resource(release)
+def get_release_resource(repo, release)
   github.releases(repo).select { |r| r.tag_name == release}.first
 end
 
-def release
-  "v#{File.read('./msi-file/version').chomp}"
+def get_version repo
+  base = File.basename repo
+  dir = base.gsub("release", "msi-file")
+  version = File.read("./#{dir}/version").chomp
+  "v#{version}"
 end
 
 def create_cloudformation_release
   template = CloudformationTemplate.new(template_json: File.read('./diego-windows-msi/cloudformation.json.template'))
-  template.base_url = "https://github.com/cloudfoundry-incubator/diego-windows-msi/releases/download/#{release}"
-  template.generate_file = 'generate.exe'
-  template.msi_file = 'DiegoWindowsMSI.msi'
-  template.setup_file = 'setup.ps1'
+  base_url = "https://github.com/cloudfoundry-incubator/diego-windows-msi/releases/download/#{release}"
+  template.generator_url = "#{base_url}/generate.exe"
+  template.diego_windows_msi_url = "#{base_url}/DiegoWindows.msi"
+  template.garden_windows_msi_url = "#{base_url}/GardenWindows.msi"
+  template.setup_url = "#{base_url}/setup.ps1"
 
   Tempfile.new('cloudformation-release.json').tap do |tempfile|
     tempfile.write(template.to_json)
@@ -97,35 +92,43 @@ def create_cloudformation_release
   end
 end
 
-release_resource = get_release_resource(release)
-if release_resource then
-  puts "Update Existing Resource"
-  body = release_resource.body
-  body += "\n\n-------------\n" + cf_diego_release_text(release)
-  github.update_release(release_resource.url, { body: body })
-else
-  puts "Creating github release"
-  res = create_github_tag
-  puts "Created github release"
+repos = [
+  "github.com/cloudfoundry-incubator/diego-windows-release",
+  "github.com/cloudfoundry-incubator/garden-windows-release"
+]
 
-  puts "Uploading generate to github release"
-  upload_release_assets generate_file, res, "generate.exe"
-  puts "Uploaded generate to github release"
+repos.each do |repo|
+  version = get_version repo
+  release_resource = get_release_resource version
+  if release_resource then
+    puts "Update Existing Resource"
+    body = release_resource.body
+    body += "\n\n-------------\n" + cf_diego_release_text(release)
+    github.update_release(release_resource.url, { body: body })
+  else
+    puts "Creating github release"
+    res = create_github_tag repo, version
+    puts "Created github release"
 
-  puts "Uploading msi to github release"
-  upload_release_assets msi_file, res, "DiegoWindowsMSI.msi"
-  puts "Uploaded msi to github release"
+    puts "Uploading generate to github release"
+    upload_release_assets generate_file, res, "generate.exe"
+    puts "Uploaded generate to github release"
 
-  puts "Uploading installation instructions to github release"
-  upload_release_assets "diego-windows-msi/docs/INSTALL.md", res
-  puts "Uploaded installation instructions to github release"
+    puts "Uploading diego windows msi to github release"
+    upload_release_assets diego_windows_msi_file, res, "DiegoWindowsMSI.msi"
+    puts "Uploaded diego windows msi to github release"
 
-  puts "Uploading setup script to github release"
-  upload_release_assets "diego-windows-msi/scripts/setup.ps1", res
-  puts "Uploaded setup script to github release"
+    puts "Uploading garden windows msi to github release"
+    upload_release_assets garden_windows_msi_file, res, "DiegoWindowsMSI.msi"
+    puts "Uploaded garden windows msi to github release"
 
-  puts "Uploading cloudformation script to github release"
-  tmp_cloudformation_file = create_cloudformation_release
-  upload_release_assets tmp_cloudformation_file.path, res, "cloudformation.json"
-  puts "Uploaded cloudformation script to github release"
+    puts "Uploading setup script to github release"
+    upload_release_assets "garden-windows-release/scripts/setup.ps1", res
+    puts "Uploaded setup script to github release"
+
+    puts "Uploading cloudformation script to github release"
+    tmp_cloudformation_file = create_cloudformation_release
+    upload_release_assets tmp_cloudformation_file.path, res, "cloudformation.json"
+    puts "Uploaded cloudformation script to github release"
+  end
 end
