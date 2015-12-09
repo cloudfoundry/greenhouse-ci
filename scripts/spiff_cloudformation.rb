964361affca8630a9e4768fbe11b3c2f3fbcfd68
Anthony Emengo <aemengo@pivotal.io>
2015-12-09 16:11:34 -0500

    Fix variable naming in spiff cloudformation script
    [#109193616]
    
    Signed-off-by: Micah Young <myoung@pivotal.io>

--- scripts/spiff_cloudformation.rb
+++ scripts/spiff_cloudformation.rb
@@ -7,7 +7,7 @@ template                        = CloudformationTemplate.new(template_json: File
 template.generator_url          = File.read("greenhouse-install-script-generator-file/url")
 template.diego_windows_msi_url  = File.read("diego-windows-msi-file/url")
 template.garden_windows_msi_url = File.read("garden-windows-msi-file/url")
-template.setup_ps1_url              = File.read("garden-windows-setup-file/url")
+template.setup_ps1_url          = File.read("garden-windows-setup-file/url")
 template.hakim_url              = File.read("hakim/url")
 
-File.write(template_file, template.to_json)
+File.write(template_file_path, template.to_json)
