e7e7ee607146fdd21f74c1fe74a9d6c5535a9896
Ben Moss <bmoss@pivotal.io>
2016-02-16 10:04:24 -0500

    Copy the output artifacts to an output directory
    Signed-off-by: Matthew Horan <mhoran@pivotal.io>

--- scripts/create-cf-release.sh
+++ scripts/create-cf-release.sh
@@ -4,3 +4,4 @@ set -ex
 
 cd cf-release
 bosh create release --force --with-tarball --name cf --version 220+dev.`date +"%s"`
+mv dev_releases/cf/cf-*.tgz ../created-cf-release
--- scripts/generate-manifest.sh
+++ scripts/generate-manifest.sh
@@ -3,5 +3,5 @@
 set -ex
 
 greenhouse-private/$DEPLOYMENT_NAME/generate-cf-diego-manifests.sh
-cp /tmp/cf.yml .
-cp /tmp/diego.yml .
+cp /tmp/cf.yml generate-manifest
+cp /tmp/diego.yml generate-manifest
