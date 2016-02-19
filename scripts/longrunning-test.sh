55e46badcc5845761481ef03d1abbb9e75c8c9aa
Ben Moss <bmoss@pivotal.io>
2016-02-19 11:04:27 -0500

    Stop posting longrunning results to a tracker that nobody pays attention to

--- scripts/longrunning-test.sh
+++ scripts/longrunning-test.sh
@@ -6,16 +6,6 @@ appname=`mktemp noraXXXXX`
 domain=greenhouse-development2.cf-app.com
 url=$appname.$domain
 
-function post_comment {
-    if [ $? -eq 0 ]; then
-        return
-    fi
-    cf d $appname -r -f >/dev/null 2>&1 || echo "could not kill app"
-    curl -X POST -H "X-TrackerToken: ${TRACKER_TOKEN}" -H "Content-Type: application/json" \
-         -d '{"text":"build failed"}' \
-         "https://www.pivotaltracker.com/services/v5/projects/${TRACKER_PROJECT_ID}/stories/${TRACKER_STORY_ID}/comments"
-}
-
 # Assumes diego-windows-msi under ./diego-windows-msi
 export nora_dir=$PWD/wats
 function cf {
@@ -23,8 +13,6 @@ function cf {
 }
 
 export -f cf
-
-trap "post_comment" EXIT
 
 cf api --skip-ssl-validation api.$domain
 cf login -u $CF_USERNAME -p $CF_PASSWORD -o ORG -s SPACE
