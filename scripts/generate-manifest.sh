c6e24c704c88176da1116bcc878002d9d15fab83
Matthew Horan <mhoran@pivotal.io>
2016-03-29 15:32:31 -0400

    Use the diego-github-release to generate diego manifest

--- scripts/generate-manifest.sh
+++ scripts/generate-manifest.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+mkdir diego-release
+tar xf diego-github-release/diego-release-*.tar.gz -C diego-release --strip-components=1
 greenhouse-private/$DEPLOYMENT_NAME/generate-cf-diego-manifests.sh
 cp /tmp/cf.yml generate-manifest
 cp /tmp/diego.yml generate-manifest
