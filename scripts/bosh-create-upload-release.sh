a2337494d207f39c3fe26c328d6338a77e192205
Brandon Shroyer <bshroyer@pivotal.io>
2016-06-23 17:05:44 -0400

    Add windows-wats pipeline
    [#121899667]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

---
+++ scripts/bosh-create-upload-release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+bosh_cli() {
+  gem install bosh_cli --no-rdoc --no-ri
+}
+
+bosh_release() {
+  pushd release
+  bosh create release --name ${RELEASE_NAME} --force
+  bosh target ${BOSH_TARGET_URL}
+  bosh login ${BOSH_USER} ${BOSH_PASSWORD}
+  bosh -t ${BOSH_TARGET_URL} upload release
+  popd
+}
+
+main() {
+  set +e
+  set -o pipefail
+
+  bosh_cli
+  bosh_release
+}
+
+main "$@"
---
+++ scripts/bosh-windows-deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+bosh_cli() {
+  gem install bosh_cli --no-rdoc --no-ri
+}
+
+bosh_deploy() {
+  bosh target ${BOSH_TARGET_URL} ${BOSH_TARGET_NAME}
+  bosh login ${BOSH_USER} ${BOSH_PASSWORD}
+  bosh -t ${BOSH_TARGET_NAME} deployment greenhouse-private/bosh-windows-manifests/garden-windows-${BOSH_TARGET_NAME}.yml
+  bosh -t ${BOSH_TARGET_NAME} -n deploy
+}
+
+main() {
+  set +e
+  set -o pipefail
+
+  bosh_cli
+  bosh_deploy
+}
+
+main "$@"
