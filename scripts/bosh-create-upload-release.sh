5e75f6052f690a3978070eca4b87d59f44b9c842
Ben Moss <bmoss@pivotal.io>
2016-06-27 09:55:01 -0400

    Add rebase when uploading release
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- scripts/bosh-create-upload-release.sh
+++ scripts/bosh-create-upload-release.sh
@@ -9,9 +9,7 @@ bosh_release() {
   bosh create release --name ${RELEASE_NAME} --force
   bosh target ${BOSH_TARGET_URL}
   bosh login ${BOSH_USER} ${BOSH_PASSWORD}
-  local release_version=$(find dev_releases/*/*+dev*.yml | xargs basename | sed "s/\.yml//" | sed "s/^${RELEASE_NAME}\-//")
-  bosh -t ${BOSH_TARGET_URL} -n delete release ${RELEASE_NAME} ${release_version}
-  bosh -t ${BOSH_TARGET_URL} upload release
+  bosh -t ${BOSH_TARGET_URL} upload release --rebase
   popd
 }
 
