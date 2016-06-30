2fe196b00b7821e55e654dc8b6a93677aaecbe65
Amin Jamali <ajamali@pivotal.io>
2016-06-30 16:12:29 -0400

    Run WATS on vsphere

--- scripts/bosh-create-upload-release.sh
+++ scripts/bosh-create-upload-release.sh
@@ -4,10 +4,19 @@ bosh_cli() {
   gem install bosh_cli --no-rdoc --no-ri
 }
 
+bosh_cert() {
+  CA_CERT=""
+  if [[ -n "${BOSH_TARGET_CERT}" ]]; then
+    echo ${BOSH_TARGET_CERT} > /tmp/cert
+    chmod 600 /tmp/cert
+    CA_CERT="--ca-cert /tmp/cert"
+  fi
+}
+
 bosh_release() {
   pushd release
   bosh create release --name ${RELEASE_NAME} --force
-  bosh target ${BOSH_TARGET_URL}
+  bosh ${CA_CERT} target ${BOSH_TARGET_URL}
   bosh login ${BOSH_USER} ${BOSH_PASSWORD}
   bosh -t ${BOSH_TARGET_URL} upload release --rebase
   popd
@@ -18,6 +27,7 @@ main() {
   set -o pipefail
 
   bosh_cli
+  bosh_cert
   bosh_release
 }
 
--- scripts/bosh-windows-deploy.sh
+++ scripts/bosh-windows-deploy.sh
@@ -4,8 +4,17 @@ bosh_cli() {
   gem install bosh_cli --no-rdoc --no-ri
 }
 
+bosh_cert() {
+  CA_CERT=""
+  if [[ -n "${BOSH_TARGET_CERT}" ]]; then
+    echo ${BOSH_TARGET_CERT} > /tmp/cert
+    chmod 600 /tmp/cert
+    CA_CERT="--ca-cert /tmp/cert"
+  fi
+}
+
 bosh_deploy() {
-  bosh target ${BOSH_TARGET_URL} ${BOSH_TARGET_NAME}
+  bosh ${CA_CERT} target ${BOSH_TARGET_URL} ${BOSH_TARGET_NAME}
   bosh login ${BOSH_USER} ${BOSH_PASSWORD}
   bosh -t ${BOSH_TARGET_NAME} deployment greenhouse-private/bosh-windows-manifests/garden-windows-${BOSH_TARGET_NAME}.yml
   bosh -t ${BOSH_TARGET_NAME} -n deploy
@@ -16,6 +25,7 @@ main() {
   set -o pipefail
 
   bosh_cli
+  bosh_cert
   bosh_deploy
 }
 
