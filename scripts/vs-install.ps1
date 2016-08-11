d81fb3d51e4c781e5d28e204258fee4c676b02c8
Dave Goddard <dave@goddard.id.au>
2016-08-11 08:32:34 -0400

    Install Visual Studio on secondary drive
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- scripts/vs-install.ps1
+++ scripts/vs-install.ps1
@@ -3,14 +3,14 @@ trap {
   exit 1
 }
 
-$env:PATH = $env:Path + ";C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE"
+$env:PATH = $env:Path + ";D:\Programs\VisualStudio\Common7\IDE"
 
 # Install Visual Studio if it doesn't exist
 if ((Get-Command "WDExpress.exe" -ErrorAction SilentlyContinue) -eq $null) {
   Write-Host "Installing Visual Studio Express 2013"
   Invoke-WebRequest "https://download.microsoft.com/download/9/6/4/96442E58-C65C-4122-A956-CCA83EECCD03/wdexpress_full.exe" -OutFile "vs-express-setup.exe"
 
-  $p = Start-Process -FilePath "vs-express-setup.exe" -ArgumentList "/silent /passive /norestart" -Wait -PassThru
+  $p = Start-Process -FilePath "vs-express-setup.exe" -ArgumentList "/silent /passive /norestart /CustomInstallPath D:\Programs\VisualStudio" -Wait -PassThru
 
   if($p.ExitCode -ne 0) {
     throw "Visual Studio Express 2013 installation process returned error code: $($p.ExitCode)"
