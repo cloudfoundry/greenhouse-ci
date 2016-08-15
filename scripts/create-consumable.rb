acf83f71277f2a29de7de0d534ba3cce1eabab74
Amin Jamali <ajamali@pivotal.io>
2016-08-15 08:48:08 -0400

    Fix create-consumable script
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- scripts/create-consumable.rb
+++ scripts/create-consumable.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 require 'json'
 
+dirs = %w(bosh-agent-zip bosh-agent-deps-zip windows-updates-list)
+
 urls = dirs.map do |dir|
   [dir, File.read("#{dir}/url").chomp]
 end
 urls = Hash[urls]
+urls['bosh-agent'] = `cd bosh-agent; git rev-parse HEAD`.chomp
 
 version = File.read('bosh-windows-shipit-version/version').chomp
 filename = "bosh-windows-client-consumable/bosh-windows-#{version}.json"
