f2eae3f485975ed4f587fce9e36a192364cac88d
Sunjay Bhatia <sbhatia@pivotal.io>
2016-08-15 10:27:40 -0400

    Adding extract-consumable
    [#127651779]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

---
+++ scripts/extract-consumable.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+require 'json'
+
+dirs = %w(bosh-agent-zip bosh-agent-deps-zip windows-updates-list)
+gits = %w(bosh-agent-sha)
+
+bosh_windows_versions= JSON.load(File.read("bosh-windows/*.json")
+dirs.each do |dir|
+  File.write("#{dir}/url", bosh_windows_versions[dir])
+end
+
+gits.each do |git|
+  File.write("#{git}/sha",bosh_windows_versions[git])
+end
