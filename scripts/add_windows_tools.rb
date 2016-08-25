b44c0bea5c56ea9af556f5b5381056f65d82c092
Ben Moss <bmoss@pivotal.io>
2016-08-25 15:40:14 -0400

    Correctly write modified manifest with windows tools
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- scripts/add_windows_tools.rb
+++ scripts/add_windows_tools.rb
@@ -2,14 +2,14 @@
 
 require 'yaml'
 
-yml = YAML.load_file(ARGV[0])
+yml = YAML.load(ARGV[0])
 
-yml['releases'].append('name' => 'windows-tools-release',
-                       'version' => 'latest')
+yml['releases'].push('name' => 'windows-tools-release',
+                     'version' => 'latest')
 
 yml['jobs'].each do |j|
   j['templates'].push('name' => 'longrunning-server',
                       'release' => 'windows-tools-release')
 end
 
-YAML.dump(yml, $stdout)
+File.open(ARGV[1], 'w') { |file| YAML.dump(yml, file) }
