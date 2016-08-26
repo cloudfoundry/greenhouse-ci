a2da9e4aaea992f2af9392d5e71c4ced93fbc866
Ben Moss <bmoss@pivotal.io>
2016-08-26 11:31:18 -0400

    Add an elastic ip to the longrunning cell
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- scripts/add_windows_tools.rb
+++ scripts/add_windows_tools.rb
@@ -7,9 +7,13 @@ yml = YAML.load_file(ARGV[0])
 yml['releases'].push('name' => 'windows-tools-release',
                      'version' => 'latest')
 
+yml['networks'].push('name' => 'longrunning-cell', 'type' => 'vip')
+
 yml['jobs'].each do |j|
   j['templates'].push('name' => 'longrunning-server',
                       'release' => 'windows-tools-release')
 end
+yml['jobs'].first['networks'].push('name' => 'longrunning-cell',
+                                   'static_ips' => ['54.87.171.179'])
 
 File.open(ARGV[1], 'w') { |file| YAML.dump(yml, file) }
