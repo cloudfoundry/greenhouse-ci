ed9f00ae9d5d2f09ace9d6a54077eed14806cd3b
Charlie Vieth <cviethjr@pivotal.io>
2016-08-26 13:53:10 -0400

    Remove elastic ip changes to longrunning
    Signed-off-by: Ben Moss <bmoss@pivotal.io>

--- scripts/add_windows_tools.rb
+++ scripts/add_windows_tools.rb
@@ -7,14 +7,9 @@ yml = YAML.load_file(ARGV[0])
 yml['releases'].push('name' => 'windows-tools-release',
                      'version' => 'latest')
 
-yml['networks'].push('name' => 'longrunning-cell', 'type' => 'vip')
-
 yml['jobs'].each do |j|
   j['templates'].push('name' => 'longrunning-server',
                       'release' => 'windows-tools-release')
 end
-yml['jobs'].first['networks'].first['default'] = ['dns', 'gateway']
-yml['jobs'].first['networks'].push('name' => 'longrunning-cell',
-                                   'static_ips' => ['54.87.171.179'])
 
 File.open(ARGV[1], 'w') { |file| YAML.dump(yml, file) }
