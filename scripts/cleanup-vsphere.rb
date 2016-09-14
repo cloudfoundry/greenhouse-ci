89d77c7a90862104c4f1498b1949e2d5ece36e23
Sunjay Bhatia <sbhatia@pivotal.io>
2016-09-14 12:12:58 -0400

    Ensure vSphere cleanup removes the vm datastore dir
    Signed-off-by: Charlie Vieth <cviethjr@pivotal.io>

--- scripts/cleanup-vsphere.rb
+++ scripts/cleanup-vsphere.rb
@@ -6,6 +6,7 @@ PACKER_VM_NAME = 'packer-vmware-iso'
 REMOTE_HOST = ENV.fetch('REMOTE_HOST')
 REMOTE_USERNAME = ENV.fetch('REMOTE_USERNAME')
 REMOTE_PASSWORD = ENV.fetch('REMOTE_PASSWORD')
+REMOTE_DATASTORE = ENV.fetch('REMOTE_DATASTORE')
 
 def find_vmid (vms,vm_name)
   packer_present = false
@@ -40,4 +41,8 @@ Net::SSH.start(REMOTE_HOST, REMOTE_USERNAME, :password => REMOTE_PASSWORD) do |s
 
   ssh.exec!("vim-cmd vmsvc/power.off #{vm_id}")
   ssh.exec!("vim-cmd vmsvc/destroy #{vm_id}")
+
+  # Check if vm data is still in datastore and remove if so
+  vm_datastore_dir = "/vmfs/volumes/#{REMOTE_DATASTORE}/#{PACKER_VM_NAME}"
+  puts ssh.exec!("if [ -d #{vm_datastore_dir} ]; then rm -r #{vm_datastore_dir}; fi")
 end
