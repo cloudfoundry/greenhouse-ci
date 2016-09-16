091a04a8a40c57122ec1c535f50a8d84ab4e59b3
Sunjay Bhatia <sbhatia@pivotal.io>
2016-09-16 13:05:56 -0400

    Zip deps as a flat archive

--- scripts/collect-and-zip-agent-deps.sh
+++ scripts/collect-and-zip-agent-deps.sh
@@ -7,7 +7,7 @@ DEPS_VERSION=$(cat version/number | tr -d '[[:space:]]')
 mv tar/tar-*.exe tar/tar.exe
 mv zlib1/zlib1-*.dll zlib1/zlib1.dll
 
-zip "bosh-agent-deps-zip/agent-dependencies-v$DEPS_VERSION.zip" \
+zip -j "bosh-agent-deps-zip/agent-dependencies-v$DEPS_VERSION.zip" \
 	bosh-blobstore-dav/bosh-blobstore-dav-*.exe \
 	bosh-blobstore-s3/bosh-blobstore-s3-*.exe \
 	job-service-wrapper/job-service-wrapper-*.exe \
