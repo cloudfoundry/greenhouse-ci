921aa6625b621c7fdef6159732f553b8723149c4
Amin Jamali <ajamali@pivotal.io>
2016-09-23 15:00:51 -0400

    Agent deps collect/zip task uses winsw resource
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- scripts/collect-and-zip-agent-deps.sh
+++ scripts/collect-and-zip-agent-deps.sh
@@ -4,12 +4,15 @@ set -ex
 
 DEPS_VERSION=$(cat version/number | tr -d '[[:space:]]')
 
-mv tar/tar-*.exe tar/tar.exe
-mv zlib1/zlib1-*.dll zlib1/zlib1.dll
+mv tar/tar{-*,}.exe
+mv zlib1/zlib1{-*,}.dll
+mv winsw-exe/{winsw-*,job-service-wrapper}.exe
+mv bosh-blobstore-dav/bosh-blobstore-dav{-*,}.exe
+mv bosh-blobstore-s3/bosh-blobstore-s3{-*,}.exe
 
 zip -j "bosh-agent-deps-zip/agent-dependencies-v$DEPS_VERSION.zip" \
-	bosh-blobstore-dav/bosh-blobstore-dav-*.exe \
-	bosh-blobstore-s3/bosh-blobstore-s3-*.exe \
-	job-service-wrapper/job-service-wrapper-*.exe \
-	tar/tar.exe \
-	zlib1/zlib1.dll
+	bosh-blobstore-dav/*.exe \
+	bosh-blobstore-s3/*.exe \
+	winsw/*.exe \
+	tar/*.exe \
+	zlib1/*.dll
