7eb7f3eccbaa57e58ddf26c634665347f99a3f2c
Amin Jamali <ajamali@pivotal.io>
2016-10-04 08:14:56 -0400

    Use the correct dav,s3 cli names
    Signed-off-by: Dave Goddard <dave@goddard.id.au>

--- scripts/collect-and-zip-agent-deps.sh
+++ scripts/collect-and-zip-agent-deps.sh
@@ -7,8 +7,8 @@ DEPS_VERSION=$(cat version/number | tr -d '[[:space:]]')
 mv tar/tar{-*,}.exe
 mv zlib1/zlib1{-*,}.dll
 mv winsw-exe/{winsw-*,job-service-wrapper}.exe
-mv bosh-blobstore-dav/bosh-blobstore-dav{-*,}.exe
-mv bosh-blobstore-s3/bosh-blobstore-s3{-*,}.exe
+mv bosh-blobstore-dav/{davcli-*-windows-amd64,bosh-blobstore-dav}.exe
+mv bosh-blobstore-s3/{s3cli-*-windows-amd64,bosh-blobstore-s3}.exe
 
 zip -j "bosh-agent-deps-zip/agent-dependencies-v$DEPS_VERSION.zip" \
 	bosh-blobstore-dav/*.exe \
