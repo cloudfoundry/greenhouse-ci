65e518a50d340f0f9e3258bfbbbedddae5e8404d
Sunjay Bhatia <sbhatia@pivotal.io>
2016-10-18 17:28:56 -0400

    Add task to collect ami, vpc, subnet for multiple regions
    [#132704567]

---
+++ scripts/collect-stemcell-region-amis.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+regions = []
+Dir.glob('windows-ami-*').each do |ami_dir|
+	region_info = {}
+	region_info["name"] = ami_dir.sub("windows-ami-", "")
+	region_info["base_ami"] = File.read(File.join(ami_dir, 'version')).chomp
+
+	region_var_name = region_info["name"].upcase.gsub("-", "_")
+	region_info["vpc_id"] = ENV.fetch("VPC_ID_"+region_var_name)
+	region_info["subnet_id"] = ENV.fetch("SUBNET_ID_"+region_var_name)
+
+	regions.push(region_info)
+end
+
+File.open("stemcell-regions/amis.json","w") do |f|
+  f.write(regions.to_json)
+end
