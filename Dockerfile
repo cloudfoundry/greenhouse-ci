7b25958c13278284818866e1e79ffc441751dbee
Amin Jamali <ajamali@pivotal.io>
2016-12-07 15:32:08 -0500

    Adding ami-status for checking if AMI is available
    [#135452985]

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,10 @@ RUN gem install bosh_cli
 RUN wget -q https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O /usr/local/bin/jq && chmod +x /usr/local/bin/jq
 RUN apt-get update && apt-get install -y python-pip zip
 RUN pip install awscli
+
+# required for https://github.com/cloudfoundry-incubator/greenhouse-ci/tree/master/scripts/ami_status.rb
+RUN gem install aws-sdk
+
 RUN echo $(jq --version && bosh --version && aws --version)
 
 
---
+++ scripts/ami-status.rb
@@ -0,0 +1,31 @@
+require 'aws-sdk'
+require 'json'
+
+class AMIStatus
+  def initialize(aws_credentials:,amis:)
+    @aws_credentials= aws_credentials
+    @amis = amis
+  end
+
+  def ami_ready(ami)
+    ec2 = Aws::EC2::Client.new(region: ami["region"],credentials: @aws_credentials)
+    begin
+      image_results = ec2.describe_images(image_ids: [ ami["ami_id"] ])
+      return (image_results.images[0].state == "available")
+    rescue
+      puts "#{ami["region"]}: #{ami["ami_id"]} is not ready "
+      return false
+    end
+  end
+
+  def ready
+    if !@amis.map { |a| ami_ready(a) }.reduce(:&)
+      abort
+    end
+  end
+
+end
+
+file = File.read(File.join("amis","amis.json"))
+cred = Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID'), ENV.fetch('AWS_SECRET_ACCESS_KEY'))
+AMIStatus.new(aws_credentials: cred, amis:JSON.parse(file)).ready
