1f4037538beb30e02525b20fdce6336fb224d568
sunjayBhatia <sbhatia@pivotal.io>
2017-01-19 02:02:49 -0800

    Fix set version since were using build not rc now

--- scripts/set-stemcell-version.rb
+++ scripts/set-stemcell-version.rb
@@ -14,7 +14,7 @@ final_version = File.read('version/number').match(/\d+\.\d+/)[0]
 stemcell_mf["version"] = final_version
 File.open("output-stemcell/stemcell.MF", "w") {|f| f.write stemcell_mf.to_yaml }
 
-stemcell_name = File.basename(stemcell[0]).gsub(/\d+\.\d+\.\d+-rc\.\d+/, final_version)
+stemcell_name = File.basename(stemcell[0]).gsub(/\d+\.\d+\.\d+-build\.\d+/, final_version)
 Dir.chdir "output-stemcell" do
   `tar czvf ../final-stemcell/#{stemcell_name} *`
 end
