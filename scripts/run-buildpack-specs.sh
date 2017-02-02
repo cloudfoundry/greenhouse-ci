64eadbaf4dd8a706075ca1ecd8c0d79286c7da1a
David Jahn <djahn@pivotal.io>
2017-02-02 15:12:18 -0500

    add no-cache to bundle install for buildpack spec runner
    Signed-off-by: Matthew Horan <mhoran@pivotal.io>

--- scripts/run-buildpack-specs.sh
+++ scripts/run-buildpack-specs.sh
@@ -4,7 +4,7 @@ set -e
 
 cd buildpack
 
-bundle install
+bundle install --jobs="$(nproc)" --no-cache
 
 bundle exec buildpack-build --uncached --stack=$STACK --host=$HOST
 bundle exec buildpack-build --cached --stack=$STACK --host=$HOST
