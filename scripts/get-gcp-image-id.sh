3c0d781ee01a354202d5599d210cb50d063b7251
Sunjay Bhatia <sbhatia@pivotal.io>
2017-02-21 18:45:24 -0500

    WIP gcp get image id
    [#140360055]

--- scripts/get-gcp-image-id.sh
+++ scripts/get-gcp-image-id.sh
@@ -9,7 +9,17 @@ gcloud auth activate-service-account --quiet $ACCOUNT_EMAIL --key-file <(echo $A
 
 set -x
 
-image_id=$(gcloud compute images list --regexp windows-server-2012-r2-dc-v.* --format json | jq -r .[0].name)
+base_image_regex=""
+if [ "$BASE_OS"  == "windows2012R2" ]; then
+  base_image_regex="windows-server-2012-r2-dc-v.*"
+elif [ "$BASE_OS" == "windows2016" ]; then
+  base_image_regex="windows-server-2016-dc-v.*"
+else
+  echo "Define BASE_OS environment variable (e.g. windows2012R2,windows2016)" 1>&2
+	exit 1
+fi
+
+image_id=$(gcloud compute images list --regexp ${base_image_regex} --format json | jq -r .[0].name)
 
 printf $image_id > image/id
 
