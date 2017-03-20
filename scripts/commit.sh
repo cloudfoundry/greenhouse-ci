e111686b78b7569acf0df4343dbf2cdc8781b286
Mark DeLillo <mdelillo@pivotal.io>
2017-03-20 14:03:39 -0400

    Copy modified repo to task output directory
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -2,8 +2,9 @@
 
 set -ex
 
-cp -r repo/* fork_repo
+cp -r destination_repo/. destination_repo_with_commit
+cp -r source_repo/* destination_repo_with_commit
 git config --global user.email "pivotal-netgarden-eng@pivotal.io"
 git config --global user.name "Greenhouse CI"
-git -C fork_repo add -A .
-git -C fork_repo commit -m "$MESSAGE"
+git -C destination_repo_with_commit add -A .
+git -C destination_repo_with_commit commit -m "$MESSAGE"
