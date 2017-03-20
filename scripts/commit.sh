48073aae798b8ca960691a754fbc56806e1ab0e6
Mark DeLillo <mdelillo@pivotal.io>
2017-03-20 11:59:36 -0400

    Configure git username and email before committing
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -3,5 +3,7 @@
 set -ex
 
 cp -r repo/* fork_repo
+git config --global user.email "pivotal-netgarden-eng@pivotal.io"
+git config --global user.name "Greenhouse CI"
 git -C fork_repo add -A .
 git -C fork_repo commit -m "$MESSAGE"
