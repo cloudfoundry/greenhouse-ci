489e0aa2be3bef043f52123c4babfef67f3bb545
Sunjay Bhatia <sbhatia@pivotal.io>
2017-03-23 16:24:13 -0400

    Make test-bosh-psmodules test more modules
    Temporary fix until we figure out how to make pester run recursively on all our modules
    Add modules with tests to the  variable
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- scripts/test-bosh-psmodules.ps1
+++ scripts/test-bosh-psmodules.ps1
@@ -1,5 +1,10 @@
 # Do not set error action preference let Pester handle it instead
 
 Import-Module ./stemcell-builder/src/github.com/pester/Pester/pester.psm1
-cd ./stemcell-builder/bosh-psmodules/modules/BOSH.Agent/
-Invoke-Pester -EnableExit
+$modules = "BOSH.Agent","BOSH.Sysprep"
+foreach ($module in $modules) {
+  Push-Location "./stemcell-builder/bosh-psmodules/modules/$module"
+    Invoke-Pester -EnableExit
+  Pop-Location
+}
+
