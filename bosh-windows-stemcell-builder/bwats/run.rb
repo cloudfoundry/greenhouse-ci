d2997aef83014b7265189618cc9e7a8fc1b96701
David Jahn <djahn@pivotal.io>
2017-03-29 15:52:34 -0400

    switch to rake task for running BWATS [#141644761]
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- bosh-windows-stemcell-builder/bwats/run.rb
+++ bosh-windows-stemcell-builder/bwats/run.rb
@@ -4,7 +4,10 @@ require 'fileutils'
 
 require_relative '../../../stemcell-builder/lib/exec_command'
 
+ENV["STEMCELL_PATH"] = File.absolute_path(ENV["STEMCELL_PATH"])
+
 Dir.chdir 'stemcell-builder' do
   exec_command('bundle install')
-  exec_command('rake test:run_bwats')
+  exec_command('rake package:bwats')
+  exec_command("rake run:bwats[#{ENV["IAAS"]}]")
 end
