930c6234a654a92d66a960e91d245163741a3a63
Charlie Vieth <cviethjr@pivotal.io>
2017-03-29 17:50:52 -0400

    Azure: use exec_command from BWSB
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- bosh-windows-stemcell-builder/create-azure-stemcell/run.rb
+++ bosh-windows-stemcell-builder/create-azure-stemcell/run.rb
@@ -1,21 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'fileutils'
-require 'open3'
 
-def exec_command(cmd)
-  STDOUT.sync = true
-  puts "Running: #{cmd}"
-  Open3.popen2(cmd) do |stdin, out, wait_thr|
-    out.each_line do |line|
-      puts line
-    end
-    exit_status = wait_thr.value
-    if exit_status != 0
-      raise "error running command: #{cmd}"
-    end
-  end
-end
+require_relative '../../../stemcell-builder/lib/exec_command'
 
 FileUtils.mkdir_p(File.join('stemcell-builder','build'))
 FileUtils.cp_r('windows-stemcell-dependencies', File.join('stemcell-builder','build','windows-stemcell-dependencies'))
