f840c1d0a4b26257b3f2b9df13cc8b5ce86b1bcd
Amin Jams <aminjam.software@gmail.com>
2017-04-04 20:50:44 -0400

    Use OAuth token for expose-gcp

--- bosh-windows-stemcell-builder/expose-gcp-image/run.rb
+++ bosh-windows-stemcell-builder/expose-gcp-image/run.rb
@@ -5,6 +5,7 @@ require 'rubygems/package'
 require 'uri'
 require 'json'
 require 'yaml'
+require 'tempfile'
 
 def read_from_tgz(path, filename)
   contents = ''
@@ -29,23 +30,34 @@ stemcell_mf = read_from_tgz(stemcell,'stemcell.MF')
 manifest = YAML.load(stemcell_mf)
 image_name = File.basename(manifest['cloud_properties']['image_url'])
 
-uri = URI.parse("https://www.googleapis.com/compute/alpha/projects/#{ENV.fetch('PROJECT_ID')}/global/images/#{image_name}/setIamPolicy?key=#{ENV.fetch('API_KEY')}")
-puts uri
-header = {'Content-Type': 'text/json'}
-data = {
-  bindings: [
-    {
-      role: 'roles/compute.imageUser',
-      members: [ 'allAuthenticatedUsers' ]
-    }
-  ]
-}
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-request = Net::HTTP::Post.new(uri.request_uri, header)
-request.body = data.to_json
 
-response = http.request(request)
-puts response.message
-exit (response.kind_of? Net::HTTPSuccess)
+account_json = JSON.parse(ENV['ACCOUNT_JSON'])
+account_email = account_json['client_email']
+project_id = account_json['project_id']
+
+Tempfile.create('bwats-') do |f|
+  f.write(ENV['ACCOUNT_JSON'])
+  f.close
+  `gcloud auth activate-service-account --quiet #{account_email} --key-file #{f.path}`
+  uri = URI.parse("https://www.googleapis.com/compute/alpha/projects/#{project_id}/global/images/#{image_name}/setIamPolicy")
+  token = `gcloud auth print-access-token`
+  header = {'Content-Type': 'application/json', 'Authorization': "Bearer #{token}"}
+  data = {
+    bindings: [
+      {
+        role: 'roles/compute.imageUser',
+        members: [ 'allAuthenticatedUsers' ]
+      }
+    ]
+  }
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Post.new(uri.request_uri, header)
+  request.body = data.to_json
+
+  response = http.request(request)
+  puts "#{response.message}: #{response.body}"
+  exit (response.kind_of? Net::HTTPSuccess)
+
+end
 
