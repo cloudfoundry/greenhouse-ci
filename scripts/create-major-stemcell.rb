662d83b3f1a44f65301a7de823731f8ca1261d09
Sunjay Bhatia <sbhatia@pivotal.io>
2017-04-05 10:20:41 -0400

    create-major-stemcell no longer needs bwats and bosh-agent

--- scripts/create-major-stemcell.rb
+++ scripts/create-major-stemcell.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/env ruby
 require 'json'
 
-dirs = %w(bosh-agent stemcell-builder bwats)
+dirs = %w(stemcell-builder
+          stemcell-builder/src/github.com/cloudfoundry/bosh-agent
+          stemcell-builder/src/github.com/cloudfoundry-incubator/bosh-windows-acceptance-tests)
 
 stemcell = Dir.glob("final-stemcell/*.tgz")[0]
 final_version = stemcell.match(/\d+\.\d+/)[0]
 
 shas = dirs.inject({}) do |acc, dir|
-  acc[dir] = `cd #{dir}; git rev-parse HEAD`.chomp
+  acc[dir.split('/').last] = `cd #{dir}; git rev-parse HEAD`.chomp
   acc
 end
 
