a37453a95495f5686a3e52d800b62458badb3659
bot <witchcraft@example.com>
2017-04-07 00:58:27 -0400

    fix git add

--- scripts/bump-submodules.sh
+++ scripts/bump-submodules.sh
@@ -8,7 +8,7 @@ pushd source-repo
   if [ -n "$(git status --porcelain)" ]; then
     git config user.email "pivotal-netgarden-eng@pivotal.io"
     git config user.name "CI (Automated)"
-    git add $SUBMODULE_PATH
+    git add .
     git commit -m "Update submodule: $(basename $SUBMODULE_PATH)"
   fi
 popd
