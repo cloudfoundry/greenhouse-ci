2def8652113e769873547f50cd9d38d90d9e6b7a
Sunjay Bhatia <sbhatia@pivotal.io>
2017-04-10 15:06:48 -0400

    simplify create-aws-stemcell task
    Signed-off-by: Paul Nikonowicz <pnikonowicz@pivotal.io>

--- bosh-windows-stemcell-builder/create-aws-stemcell/run.rb
+++ bosh-windows-stemcell-builder/create-aws-stemcell/run.rb
@@ -4,11 +4,6 @@ require 'fileutils'
 
 require_relative '../../../stemcell-builder/lib/exec_command'
 
-FileUtils.mkdir_p(File.join("stemcell-builder","build"))
-FileUtils.cp_r("windows-stemcell-dependencies", File.join("stemcell-builder","build","windows-stemcell-dependencies"))
-FileUtils.cp_r("version", File.join("stemcell-builder","build","version"))
-FileUtils.cp_r("base-amis", File.join("stemcell-builder","build","base-amis"))
-
 Dir.chdir "stemcell-builder" do
   exec_command("bundle install")
   exec_command("rake package:agent")
