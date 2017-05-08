6e2b4d56b9b902b273a456ffad74195a88f5ed46
Amin Jamali <ajamali@pivotal.io>
2017-05-08 10:57:32 -0400

    powershell catch non-modules exceptions
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- bosh-windows-stemcell-builder/create-vmx-add-updates/run.ps1
+++ bosh-windows-stemcell-builder/create-vmx-add-updates/run.ps1
@@ -2,6 +2,6 @@ $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
 cd stemcell-builder
-bundle install
-rake package:psmodules
-rake build:vsphere_add_updates
+$(bundle install) -or $(Write-Error 'Failed to install')
+$(rake package:psmodules) -or $(Write-Error 'Failed to package psmodules')
+$(rake build:vsphere_add_updates) -or $(Write-Error 'Failed to add updates to image')
--- bosh-windows-stemcell-builder/create-vsphere-stemcell/run.ps1
+++ bosh-windows-stemcell-builder/create-vsphere-stemcell/run.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
 cd "stemcell-builder"
-bundle install
-rake package:agent
-rake package:psmodules
-rake build:vsphere
+$(bundle install) -or $(Write-Error 'Failed to install')
+$(rake package:agent) -or $(Write-Error 'Failed to install')
+$(rake package:psmodules) -or $(Write-Error 'Failed to package psmodules')
+$(rake build:vsphere) -or $(Write-Error 'Failed to create vsphere stemcell')
