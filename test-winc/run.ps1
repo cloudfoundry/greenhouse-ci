b7239bc62381d79ee9c0550fe6a30d45e40b14b4
Sunjay Bhatia <sbhatia@pivotal.io>
2017-05-11 20:45:35 +0100

    Add tasks for winc
    [#145024437]

---
+++ test-winc/run.ps1
@@ -0,0 +1,31 @@
+$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$env:GOPATH = $PWD
+$env:PATH = $env:GOPATH + "/bin;C:/go/bin;" + $env:PATH
+
+if ((Get-Command "go.exe" -ErrorAction SilentlyContinue) -eq $null) {
+  Write-Host "Installing Go"
+  Invoke-WebRequest https://storage.googleapis.com/golang/go1.8.1.windows-amd64.msi -OutFile go.msi
+
+  $p = Start-Process -FilePath "msiexec" -ArgumentList "/passive /norestart /i go.msi" -Wait -PassThru
+
+  if($p.ExitCode -ne 0) {
+    throw "Golang MSI installation process returned error code: $($p.ExitCode)"
+  }
+
+  Write-Host "Installed Go"
+}
+
+go.exe version
+
+cd $env:GOPATH/src/code.cloudfoundry.org/winc
+
+Write-Host "Installing Ginkgo"
+go.exe install ./vendor/github.com/onsi/ginkgo/ginkgo
+if ($LastExitCode -ne 0) {
+    throw "Ginkgo installation process returned error code: $LastExitCode"
+}
+
+ginkgo.exe -r -race -keepGoing
+Exit $LastExitCode
