b66c403cf5684a06221bbf26fa74377ed3c6ff12
Sunjay Bhatia <sunjayBhatia@users.noreply.github.com>
2017-05-29 08:44:51 -0400

    update path before checking for go.exe

--- build-winc/run.ps1
+++ build-winc/run.ps1
@@ -1,6 +1,9 @@
 $ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
+$env:GOPATH = $PWD
+$env:PATH = $env:GOPATH + "/bin;C:/go/bin;" + $env:PATH
+
 if ((Get-Command "go.exe" -ErrorAction SilentlyContinue) -eq $null) {
   Write-Host "Installing Go"
   Invoke-WebRequest https://storage.googleapis.com/golang/go1.8.1.windows-amd64.msi -OutFile go.msi
@@ -13,9 +16,6 @@ if ((Get-Command "go.exe" -ErrorAction SilentlyContinue) -eq $null) {
 
   Write-Host "Installed Go"
 }
-
-$env:GOPATH = $PWD
-$env:PATH = $env:GOPATH + "/bin;C:/go/bin;" + $env:PATH
 
 go.exe build -o winc-binary/winc.exe code.cloudfoundry.org/winc/cmd/winc
 Exit $LastExitCode
