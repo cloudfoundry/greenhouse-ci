14c228415352cde313a0391910b805d472c62a94
Matthew Horan <mhoran@pivotal.io>
2017-05-30 16:50:25 -0400

    Stringify the VMDK_PATH for creating diff disk
    Signed-off-by: Paul Nikonowicz <pnikonowicz@pivotal.io>

--- bosh-windows-stemcell-builder/create-vsphere-vmdk/run.ps1
+++ bosh-windows-stemcell-builder/create-vsphere-vmdk/run.ps1
@@ -1,6 +1,6 @@
 ﻿#create vmdk
 
-$env:VMDK_PATH=pivotal-base-image/en.2k12r2.serverdatacenter.rtm.fpp.patched.vhd
+$env:VMDK_PATH="pivotal-base-image/en.2k12r2.serverdatacenter.rtm.fpp.patched.vhd"
 
 cd "stemcell-builder"
 bundle install
