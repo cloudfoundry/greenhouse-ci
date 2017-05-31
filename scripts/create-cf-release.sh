1d23dc44c97e554ac26485d2cc7b67c5055ff395
Amin Jamali <ajamali@pivotal.io>
2017-05-31 14:15:01 -0400

    Delete un-needed tasks for MSI workflow
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- scripts/create-cf-release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-cd cf-release
-bosh create release --force --with-tarball --name cf --version 220+dev.`date +"%s"`
-mv dev_releases/cf/cf-*.tgz ../created-cf-release
--- scripts/generate-manifest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-mkdir diego-release
-tar xf diego-github-release/source.tar.gz -C diego-release --strip-components=1
-greenhouse-private/$DEPLOYMENT_NAME/generate-cf-diego-manifests.sh
-cp /tmp/cf.yml generate-manifest
-cp /tmp/diego.yml generate-manifest
