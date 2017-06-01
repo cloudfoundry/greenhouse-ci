64faec9358028651c5ca1c6ab3bc4da8802848f1
Sunjay Bhatia <sunjayBhatia@users.noreply.github.com>
2017-05-31 22:39:47 -0400

    bump to go 1.8.3

--- build-winc/run.ps1
+++ build-winc/run.ps1
@@ -6,7 +6,7 @@ $env:PATH = $env:GOPATH + "/bin;C:/go/bin;" + $env:PATH
 
 if ((Get-Command "go.exe" -ErrorAction SilentlyContinue) -eq $null) {
   Write-Host "Installing Go"
-  Invoke-WebRequest https://storage.googleapis.com/golang/go1.8.1.windows-amd64.msi -OutFile go.msi
+  Invoke-WebRequest https://storage.googleapis.com/golang/go1.8.3.windows-amd64.msi -OutFile go.msi
 
   $p = Start-Process -FilePath "msiexec" -ArgumentList "/passive /norestart /i go.msi" -Wait -PassThru
 
