184912c633eae36c4ca97bcc67a00304420715dd
David Jahn <djahn@pivotal.io>
2017-06-01 12:28:42 -0400

    invoke packer from vhd file [#145862779]
    Signed-off-by: Paul Nikonowicz <pnikonowicz@pivotal.io>

--- bosh-windows-stemcell-builder/create-vsphere-vmdk/run.rb
+++ bosh-windows-stemcell-builder/create-vsphere-vmdk/run.rb
@@ -6,16 +6,16 @@ version = File.read(File.join(version_dir, 'number')).chomp
 vsphere = Stemcell::Builder::VSphere.new(
   mem_size: ENV.fetch('MEM_SIZE', '4096'),
   num_vcpus: ENV.fetch('NUM_VCPUS', '8'),
-  source_path: Stemcell::Builder::validate_env('VMDK_PATH'),
+  source_path: Stemcell::Builder::validate_env('VMX_PATH'),
   agent_commit: "",
   administrator_password: Stemcell::Builder::validate_env('ADMINISTRATOR_PASSWORD'),
   product_key: Stemcell::Builder::validate_env('PRODUCT_KEY'),
   owner: Stemcell::Builder::validate_env('OWNER'),
   organization: Stemcell::Builder::validate_env('ORGANIZATION'),
   os: Stemcell::Builder::validate_env('OS_VERSION'),
-  output_directory: '../output',
+  output_directory: Stemcell::Builder::validate_env('OUTPUT_DIR'),
   packer_vars: {},
   version: version
 )
 
-vsphere.build
+vsphere.run_packer
