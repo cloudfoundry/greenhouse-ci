b673ea3b7d48bd9688e8685abaab2ead3713c858
Paul Nikonowicz <pnikonowicz@pivotal.io>
2017-06-01 11:17:52 -0400

    Revert "template for running diff on linux"
    This reverts commit 29e45eee8b43a747890627ec7f5ebb981f2a05f2.
    
    Signed-off-by: David Jahn <djahn@pivotal.io>

---
+++ bosh-windows-stemcell-builder/create-vsphere-vmdk/run.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$env:VMDK_PATH="pivotal-base-image/en.2k12r2.serverdatacenter.rtm.fpp.patched.vhd"
+
+cd "stemcell-builder"
+bundle install
+
+ruby ../ci/bosh-windows-stemcell-builder/create-vsphere-vmdk/run.rb
