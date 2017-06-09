737bf0481ef042dad272bf923ea139ec63545ad1
Matthew Horan <mhoran@pivotal.io>
2017-06-09 14:46:39 -0400

    Missing src in path
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- copy-forks/run.ps1
+++ copy-forks/run.ps1
@@ -5,7 +5,7 @@ Remove-Item -Recurse -Force garden-runc-release/src/code.cloudfoundry.org/guardi
 write-host "Copying guardian fork...."
 Copy-Item  -Recurse guardian garden-runc-release/src/code.cloudfoundry.org/
 
-Remove-Item -Recurse -Force garden-runc-release/code.cloudfoundry.org/garden-integration-tests
+Remove-Item -Recurse -Force garden-runc-release/src/code.cloudfoundry.org/garden-integration-tests
 write-host "Copying GATS fork...."
 Copy-Item  -Recurse garden-integration-tests garden-runc-release/src/code.cloudfoundry.org/
 
