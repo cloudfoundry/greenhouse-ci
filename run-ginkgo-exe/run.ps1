215ed5921f5ae73bbcdb32341c7f8e1f0d4fe4f5
Amin Jamali <ajamali@pivotal.io>
2017-06-12 14:57:07 -0400

    Expand gopath
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- run-ginkgo-exe/run.ps1
+++ run-ginkgo-exe/run.ps1
@@ -1,5 +1,11 @@
-﻿$ErrorActionPreference = "Stop";
+﻿Set-PSDebug -trace 1            # "set -x"
+Set-PSDebug -strict             # "set -u"
+$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
+
+echo "OLD GOPATH: [$env:GOPATH]"
+$env:GOPATH=(Get-ChildItem $env:GOPATH).FullName
+echo "NEW GOPATH: [$env:GOPATH]"
 
 go get github.com/onsi/ginkgo/ginkgo
 
