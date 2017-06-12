409aed6c46fc84016f1dd9663af4066c5d05651a
Amin Jamali <ajamali@pivotal.io>
2017-06-12 15:12:08 -0400

    fix gopath for run-ginkgo-exe
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- run-ginkgo-exe/run.ps1
+++ run-ginkgo-exe/run.ps1
@@ -1,13 +1,9 @@
-﻿Set-PSDebug -trace 1            # "set -x"
-Set-PSDebug -strict             # "set -u"
-$ErrorActionPreference = "Stop";
+﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-echo "OLD GOPATH: [$env:GOPATH]"
-$env:GOPATH=(Get-ChildItem $env:GOPATH).FullName
-echo "NEW GOPATH: [$env:GOPATH]"
+$env:GOPATH=(Resolve-Path $env:GOPATH).Path
 
 go get github.com/onsi/ginkgo/ginkgo
 
-$env:GOPATH/bin/ginkgo.exe -p -r -race -cover -keepGoing -randomizeSuites $env:TEST_PATH
+& "$env:GOPATH/bin/ginkgo.exe" -p -r -race -cover -keepGoing -randomizeSuites $env:TEST_PATH
 Exit $LastExitCode
