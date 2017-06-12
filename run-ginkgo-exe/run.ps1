08673ea4d42c2772d6a04eb5a11dbb35d60e934e
Mark DeLillo <mdelillo@pivotal.io>
2017-06-12 15:21:37 -0400

    Run ginkgo tests relative to repo
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- run-ginkgo-exe/run.ps1
+++ run-ginkgo-exe/run.ps1
@@ -5,5 +5,6 @@ $env:GOPATH=(Resolve-Path $env:GOPATH).Path
 
 go get github.com/onsi/ginkgo/ginkgo
 
+cd repo
 & "$env:GOPATH/bin/ginkgo.exe" -p -r -race -cover -keepGoing -randomizeSuites $env:TEST_PATH
 Exit $LastExitCode
