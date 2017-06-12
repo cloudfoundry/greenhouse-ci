668954a120f167f77b4e4f0829bb41a736630bb9
Sam Smith <sesmith177@gmail.com>
2017-06-12 15:02:23 -0400

    Add wincni-smoke job

---
+++ wincni-smoke/run.ps1
@@ -0,0 +1,43 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$env:GOPATH = $PWD + cf-external-networking
+$env:PATH = $env:GOPATH + "/bin;C:/go/bin;" + $env:PATH
+
+if ((Get-Command "go.exe" -ErrorAction SilentlyContinue) -eq $null) {
+  Write-Host "Installing Go"
+  Invoke-WebRequest https://storage.googleapis.com/golang/go1.8.3.windows-amd64.msi -OutFile go.msi
+
+  $p = Start-Process -FilePath "msiexec" -ArgumentList "/passive /norestart /i go.msi" -Wait -PassThru
+
+  if($p.ExitCode -ne 0) {
+    throw "Golang MSI installation process returned error code: $($p.ExitCode)"
+  }
+
+  Write-Host "Installed Go"
+}
+
+go version
+
+Write-Host "Installing garden-external-networker"
+go install garden-external-networker
+if ($LastExitCode -ne 0) {
+    throw "garden-external-networker installation process returned error code: $LastExitCode"
+}
+
+Write-Host "creating directories"
+mkdir -p  cni-config
+mkdir -p  bindmounts
+mkdir -p  state
+
+Write-Host "bringing up network"
+gardne-external-networker -action=up -handle=XXX -configFile=ci\local-gdn-ext-net-config\config.json
+if ($LastExitCode -ne 0) {
+    throw "garden-external-networker up returned error code: $LastExitCode"
+}
+
+Write-Host "bringing down network"
+gardne-external-networker -action=down -handle=XXX -configFile=ci\local-gdn-ext-net-config\config.json
+if ($LastExitCode -ne 0) {
+    throw "garden-external-networker down returned error code: $LastExitCode"
+}
