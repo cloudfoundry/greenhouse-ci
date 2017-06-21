4d41a390eea9ddc5fc76236b8322a53d09a286e6
Amin Jamali <ajamali@pivotal.io>
2017-06-21 08:23:06 -0400

    Add build-tar task

---
+++ build-tar/run.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+# remove sh.exe from $env:PATH
+$env:PATH = ($env:PATH.Split(';') | Where-Object { $_ -ne 'c:\var\vcap\packages\git\bin' }) -join ';'
+
+bsdtar/install.ps1
+Exit $LastExitCode
