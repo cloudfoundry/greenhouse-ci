6193dc57b64adef949d72700183d4db23ecc8957
Amin Jamali <ajamali@pivotal.io>
2017-06-21 08:40:07 -0400

    [test-hwc] missing packages for 2016

--- test-hwc/run.ps1
+++ test-hwc/run.ps1
@@ -7,8 +7,6 @@ $env:PATH = $env:GOPATH + "/bin;C:/go/bin;" + $env:PATH
 Install-WindowsFeature Web-WHC
 Install-WindowsFeature Web-Webserver
 Install-WindowsFeature Web-WebSockets
-Install-WindowsFeature AS-Web-Support
-Install-WindowsFeature AS-NET-Framework
 Install-WindowsFeature Web-WHC
 Install-WindowsFeature Web-ASP
 
