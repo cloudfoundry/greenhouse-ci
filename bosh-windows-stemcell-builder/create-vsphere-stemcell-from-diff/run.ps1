d0c72557f62aceb1c3131ec8e12d03a0f25bef51
Natalie Arellano <narellano@pivotal.io>
2017-08-04 17:43:35 -0400

    Add job for creating vsphere stemcell from patchfile
    [#149951843](https://www.pivotaltracker.com/story/show/149951843)

---
+++ bosh-windows-stemcell-builder/create-vsphere-stemcell-from-diff/run.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+cd "stemcell-builder"
+bundle install
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not bundle install"
+  Exit 1
+}
+
+rake build:vsphere_from_diff
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not build vsphere stemcell from diff"
+  Exit 1
+}
