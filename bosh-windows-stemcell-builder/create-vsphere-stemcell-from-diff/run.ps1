91647768172fa8ec094010cba41087218fb55436
Natalie Arellano <narellano@pivotal.io>
2017-08-08 11:19:10 -0400

    Add back needed rake tasks
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- bosh-windows-stemcell-builder/create-vsphere-stemcell-from-diff/run.ps1
+++ bosh-windows-stemcell-builder/create-vsphere-stemcell-from-diff/run.ps1
@@ -8,6 +8,18 @@ if ($LASTEXITCODE -ne 0) {
   Exit 1
 }
 
+rake package:agent
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not package agent"
+  Exit 1
+}
+
+rake package:psmodules
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not package psmodules"
+  Exit 1
+}
+
 rake build:vsphere_diff
 if ($LASTEXITCODE -ne 0) {
   Write-Error "Could not build vsphere stemcell from diff"
