2157a629904a1d443166ae9575c26616be78c0e9
David Jahn <djahn@pivotal.io>
2017-08-09 13:48:49 -0400

    private stemcell build pipeline needs this task
    Signed-off-by: Paul Nikonowicz <pnikonowicz@pivotal.io>

---
+++ bosh-windows-stemcell-builder/create-vsphere-stemcell/run.ps1
@@ -0,0 +1,21 @@
+﻿cd "stemcell-builder"
+bundle install
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not bundle install"
+  Exit 1
+}
+rake package:agent
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not package agent"
+  Exit 1
+}
+rake package:psmodules
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not package psmodules"
+  Exit 1
+}
+rake build:vsphere
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not build vsphere"
+  Exit 1
+}
