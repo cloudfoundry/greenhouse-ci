e0e77b5d0fee555efb574f3f2334b49af7a6eed7
Natalie Arellano <narellano@pivotal.io>
2017-08-15 15:10:32 -0400

    Add missing task files
    Signed-off-by: Paul Nikonowicz <pnikonowicz@pivotal.io>

---
+++ bosh-windows-stemcell-builder/create-vmx-add-updates/run.ps1
@@ -0,0 +1,16 @@
+﻿cd stemcell-builder
+bundle install
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not bundle install"
+  Exit 1
+}
+rake package:psmodules
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not package psmodules"
+  Exit 1
+}
+rake build:vsphere_add_updates
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not install updates to vsphere image"
+  Exit 1
+}
