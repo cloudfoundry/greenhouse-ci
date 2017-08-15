0853b8f812172cac871b062b41974fe47a8e675c
Sunjay Bhatia <sbhatia@pivotal.io>
2017-08-15 12:07:55 -0400

    add task for docker-build-and-push on windows
    [#148109721]
    
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

---
+++ docker-build-and-push/run.ps1
@@ -0,0 +1,36 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+function Run-Docker {
+  param([String[]] $cmd)
+
+  docker @cmd
+  if ($LASTEXITCODE -ne 0) {
+    Exit $LASTEXITCODE
+  }
+}
+
+$env:PATH = "C:/Program Files/Docker;" + $env:PATH
+
+restart-service docker
+
+$version=(cat version/number)
+echo $version
+
+mkdir buildDir
+cp $env:DOCKERFILE buildDir\Dockerfile
+cp git-setup\Git-*-64-bit.exe buildDir\
+cp tar\tar-*.exe buildDir\
+cd buildDir
+
+Run-Docker "--version"
+
+Run-Docker "build", "-t", "$env:IMAGE_NAME", "-t", "${env:IMAGE_NAME}:$version", "."
+
+Run-Docker "images", "-a"
+
+Run-Docker "login", "-u", "$env:DOCKER_USERNAME", "-p", "$env:DOCKER_PASSWORD"
+
+Run-Docker "push", "$env:IMAGE_NAME"
+
+Run-Docker "push", "${env:IMAGE_NAME}:$version"
