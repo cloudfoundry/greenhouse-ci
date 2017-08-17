43f092c9d871d4f159b97f70d67a7019979a409d
Mark DeLillo <mdelillo@pivotal.io>
2017-08-17 09:43:08 -0400

    Include binary to set urlacl's in windows2016fs docker build
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- docker-build-and-push/run.ps1
+++ docker-build-and-push/run.ps1
@@ -20,16 +20,12 @@ mkdir buildDir
 cp $env:DOCKERFILE buildDir\Dockerfile
 cp git-setup\Git-*-64-bit.exe buildDir\
 cp tar\tar-*.exe buildDir\
+cp urlacl\urlacl.exe buildDir\
 cd buildDir
 
 Run-Docker "--version"
-
 Run-Docker "build", "-t", "$env:IMAGE_NAME", "-t", "${env:IMAGE_NAME}:$version", "."
-
 Run-Docker "images", "-a"
-
 Run-Docker "login", "-u", "$env:DOCKER_USERNAME", "-p", "$env:DOCKER_PASSWORD"
-
 Run-Docker "push", "${env:IMAGE_NAME}:latest"
-
 Run-Docker "push", "${env:IMAGE_NAME}:$version"
