9c623bf1d22e70e75414f05033d66ca8338db931
Sam Smith <sesmith177@gmail.com>
2017-08-28 14:54:50 -0400

    Download rewrite msi for windows2016 fs image
    [#150527696]
    
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- docker-build-and-push/run.ps1
+++ docker-build-and-push/run.ps1
@@ -20,6 +20,7 @@ mkdir buildDir
 cp $env:DOCKERFILE buildDir\Dockerfile
 cp git-setup\Git-*-64-bit.exe buildDir\
 cp tar\tar-*.exe buildDir\
+curl -UseBasicParsing -Outfile buildDir\rewrite.msi -Uri "https://download.microsoft.com/download/C/9/E/C9E8180D-4E51-40A6-A9BF-776990D8BCA9/rewrite_amd64.msi"
 cd buildDir
 
 Run-Docker "--version"
