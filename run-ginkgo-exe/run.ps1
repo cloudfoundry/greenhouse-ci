83898c8b5b0531d511477ff94d5aa9756992936c
Sam Smith <sesmith177@gmail.com>
2017-08-30 11:55:37 -0400

    Add go to path for run-ginkgo-exe

--- run-ginkgo-exe/run.ps1
+++ run-ginkgo-exe/run.ps1
@@ -3,6 +3,8 @@ trap { $host.SetShouldExit(1) }
 
 $env:GOPATH=(Resolve-Path $env:GOPATH).Path
 
+$env:PATH = "C:/var/vcap/packages/golang-windows/go/bin;" + $env:PATH
+
 go get github.com/onsi/ginkgo/ginkgo
 
 cd repo
