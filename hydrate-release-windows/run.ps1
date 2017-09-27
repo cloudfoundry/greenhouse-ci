44cb88b72668534e2656c346979461e67cbe9155
Sunjay Bhatia <sbhatia@pivotal.io>
2017-09-27 12:26:30 -0400

    trying to make pipeline more efficient

--- hydrate-release-windows/run.ps1
+++ hydrate-release-windows/run.ps1
@@ -1,6 +1,8 @@
 ﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
+cp ./version/version ./windows2016fs-release/IMAGE_TAG
+
 ./windows2016fs-release/scripts/hydrate.ps1
 
 cp -Recurse -Force ./windows2016fs-release/* hydrated-release
