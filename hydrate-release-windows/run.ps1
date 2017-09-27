e5f4c221ad719c2fadfe4933a49de129f7ccc428
Sunjay Bhatia <sbhatia@pivotal.io>
2017-09-27 18:12:10 -0400

    remove version update from hydrate tasks

--- hydrate-release-windows/run.ps1
+++ hydrate-release-windows/run.ps1
@@ -1,8 +1,6 @@
 ﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-cp ./version/version ./windows2016fs-release/IMAGE_TAG
-
 ./windows2016fs-release/scripts/hydrate.ps1
 
 cp -Recurse -Force ./windows2016fs-release/* hydrated-release
