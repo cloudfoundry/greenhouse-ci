dfb4b57e6a75438af5d6d406fed7a764787873dc
Sunjay Bhatia <sbhatia@pivotal.io>
2017-10-04 13:46:42 -0400

    Update CI to use new scripts/create-release
    [#151684752]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- hydrate-release-windows/run.ps1
+++
@@ -1,6 +0,0 @@
-﻿$ErrorActionPreference = "Stop";
-trap { $host.SetShouldExit(1) }
-
-./windows2016fs-release/scripts/hydrate.ps1
-
-cp -Recurse -Force ./windows2016fs-release/* hydrated-release
---
+++ test-create-fs-release-windows/run.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+./windows2016fs-release/scripts/create-release.ps1
+
+push-location windows2016fs-release
+  $expected_version=(cat VERSION)
+  $created_version=(bosh int "dev_releases/windows2016fs/windows2016fs-$expected_version.yml" --path "/version")
+  $uncommitted_changes=(bosh int "dev_releases/windows2016fs/windows2016fs-$expected_version.yml" --path "/uncommitted_changes")
+pop-location
+
+if ($expected_version -ne $created_version) {
+  echo "**ERROR** expected version: $expected_version, got: $created_version"
+  exit 1
+}
+
+if ($uncommitted_changes -ne "false") {
+  echo "**ERROR** found uncommited changes"
+  exit 1
+}
