5b1e72fa4efc3466995462021197bda29189bbb0
Sunjay Bhatia <sbhatia@pivotal.io>
2017-10-04 16:29:20 -0400

    add tar to the path in the task
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

--- test-create-fs-release-windows/run.ps1
+++ test-create-fs-release-windows/run.ps1
@@ -1,6 +1,9 @@
 ﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
+# get tar on the path
+$env:PATH="$env:PATH;C:\var\vcap\bosh\bin"
+
 push-location windows2016fs-release
   git config core.filemode false
   git submodule foreach --recursive git config core.filemode false
