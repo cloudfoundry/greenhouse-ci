631ea9612c560551803b006e533782727778466e
Sam Smith <sesmith177@gmail.com>
2017-10-04 16:48:01 -0400

    pass args to bosh int for windows with --path=<foo>
    Signed-off-by: Sunjay Bhatia <sbhatia@pivotal.io>

--- test-create-fs-release-windows/run.ps1
+++ test-create-fs-release-windows/run.ps1
@@ -11,8 +11,8 @@ push-location windows2016fs-release
   ./scripts/create-release.ps1
 
   $expected_version=(cat VERSION)
-  $created_version=(bosh int "dev_releases/windows2016fs/windows2016fs-$expected_version.yml" --path "/version")
-  $uncommitted_changes=(bosh int "dev_releases/windows2016fs/windows2016fs-$expected_version.yml" --path "/uncommitted_changes")
+  $created_version=(bosh int "dev_releases/windows2016fs/windows2016fs-$expected_version.yml" --path=/version)
+  $uncommitted_changes=(bosh int "dev_releases/windows2016fs/windows2016fs-$expected_version.yml" --path=/uncommitted_changes)
 pop-location
 
 if ($expected_version -ne $created_version) {
