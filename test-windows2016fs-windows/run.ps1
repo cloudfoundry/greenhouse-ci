b69d49dbdf67c6c95729cfa3c106ebff3ff1844e
Sam Smith <sesmith177@gmail.com>
2017-10-06 15:19:09 -0400

    Add specific jobs for windows2016fs unit tests
    [#151682649]
    
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

---
+++ test-windows2016fs-windows/run.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+$env:GOPATH=$PWD
+$env:PATH = "C:/var/vcap/packages/golang-windows/go/bin;" + $env:PATH
+
+go get github.com/onsi/ginkgo/ginkgo
+
+& "$env:GOPATH/bin/ginkgo.exe" -nodes $env:NODES -r -race -cover -keepGoing -randomizeSuites src/code.cloudfoundry.org/windows2016fs
+Exit $LastExitCode
