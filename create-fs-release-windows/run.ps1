057c0c1fd1c6a301eadf0cd93943ab22a9acd740
Sam Smith <sesmith177@gmail.com>
2017-10-11 16:45:59 -0400

    Make sure git author is set
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- create-fs-release-windows/run.ps1
+++ create-fs-release-windows/run.ps1
@@ -17,15 +17,23 @@ push-location windows2016fs-release
   }
 
   if ($env:DEV_ENV -eq $null -or $env:DEV_ENV -eq "") {
-     echo $pre_version > VERSION
-     git add VERSION
-     if ($LastExitCode -ne 0) {
-       exit $LastExitCode
-     }
-     git commit -m "WIP - test"
-     if ($LastExitCode -ne 0) {
-       exit $LastExitCode
-     }
+    echo $pre_version > VERSION
+    git config --global user.email "pivotal-netgarden-eng@pivotal.io"
+    if ($LastExitCode -ne 0) {
+      exit $LastExitCode
+    }
+    git config --global user.name "Greenhouse CI"
+    if ($LastExitCode -ne 0) {
+      exit $LastExitCode
+    }
+    git add VERSION
+    if ($LastExitCode -ne 0) {
+      exit $LastExitCode
+    }
+    git commit -m "WIP - test"
+    if ($LastExitCode -ne 0) {
+      exit $LastExitCode
+    }
   }
 
   ./scripts/create-release.ps1
