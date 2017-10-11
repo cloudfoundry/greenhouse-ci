eca8b5bef5857e9fd86528953ce1ea6a57894d1e
Mark DeLillo <mdelillo@pivotal.io>
2017-10-11 15:02:27 -0400

    Misc windows2016fs changes
    - Update blob + bump windows2016fs-release are now the same job
    - create-fs-release-* now take DEV_ENV as a variable
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

---
+++ create-fs-release-windows/run.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+# get tar on the path
+$env:PATH="$env:PATH;C:\var\vcap\bosh\bin"
+
+$pre_version=(cat version/version)
+
+push-location windows2016fs-release
+  git config core.filemode false
+  if ($LastExitCode -ne 0) {
+    exit $LastExitCode
+  }
+  git submodule foreach --recursive git config core.filemode false
+  if ($LastExitCode -ne 0) {
+    exit $LastExitCode
+  }
+
+  if ($env:DEV_ENV -eq $null -or $env:DEV_ENV -eq "") {
+     echo $pre_version > VERSION
+  }
+
+  ./scripts/create-release.ps1
+  if ($LastExitCode -ne 0) {
+    exit $LastExitCode
+  }
+pop-location
+
+if ($env:DEV_ENV -ne $null -and $env:DEV_ENV -ne "") {
+  cp windows2016fs-release/bin/create.exe "create-binary-windows/create-$pre_version-windows-amd64.exe"
+}
