8a2d61a79b4ed4a3d208db216f07725014ff5b89
Sam Smith <sesmith177@gmail.com>
2017-11-20 17:25:12 -0500

    Allow admins through firewall
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- run-local-gats/run.ps1
+++ run-local-gats/run.ps1
@@ -1,12 +1,67 @@
 ﻿$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-set-netfirewallprofile -all -defaultInboundAction block -defaultOutboundAction block
-
-function Kill-Garden
-{
+function Kill-Garden {
   Get-Process | foreach { if ($_.name -eq "gdn") { kill -Force $_.Id } }
 }
+
+
+function get-firewall {
+	param([string] $profile)
+	$firewall = (Get-NetFirewallProfile -Name $profile)
+	$result = "{0},{1},{2}" -f $profile,$firewall.DefaultInboundAction,$firewall.DefaultOutboundAction
+	return $result
+}
+
+function check-firewall {
+	param([string] $profile)
+	$firewall = (get-firewall $profile)
+	Write-Host $firewall
+	if ($firewall -ne "$profile,Block,Block") {
+		Write-Host $firewall
+		Write-Error "Unable to set $profile Profile"
+	}
+}
+
+function setup-firewall {
+  $anyFirewallsDisabled = !!(Get-NetFirewallProfile -All | Where-Object { $_.Enabled -eq "False" })
+  $adminRuleMissing = !(Get-NetFirewallRule -Name CFAllowAdmins -ErrorAction Ignore)
+  if ($anyFirewallsDisabled -or $adminRuleMissing) {
+    $admins = New-Object System.Security.Principal.NTAccount("Administrators")
+    $adminsSid = $admins.Translate([System.Security.Principal.SecurityIdentifier])
+
+    $LocalUser = "D:(A;;CC;;;$adminsSid)"
+    $otherAdmins = Get-WmiObject win32_groupuser |
+    Where-Object { $_.GroupComponent -match 'administrators' } |
+    ForEach-Object { [wmi]$_.PartComponent }
+
+    foreach($admin in $otherAdmins)
+    {
+      $ntAccount = New-Object System.Security.Principal.NTAccount($admin.Name)
+      $sid = $ntAccount.Translate([System.Security.Principal.SecurityIdentifier]).Value
+      $LocalUser = $LocalUser + "(A;;CC;;;$sid)"
+    }
+    New-NetFirewallRule -Name CFAllowAdmins -DisplayName "Allow admins" `
+      -Description "Allow admin users" -RemotePort Any `
+      -LocalPort Any -LocalAddress Any -RemoteAddress Any `
+      -Enabled True -Profile Any -Action Allow -Direction Outbound `
+      -LocalUser $LocalUser
+
+    Set-NetFirewallProfile -All -DefaultInboundAction Block -DefaultOutboundAction Block -Enabled True
+    check-firewall "public"
+    check-firewall "private"
+    check-firewall "domain"
+    $anyFirewallsDisabled = !!(Get-NetFirewallProfile -All | Where-Object { $_.Enabled -eq "False" })
+    $adminRuleMissing = !(Get-NetFirewallRule -Name CFAllowAdmins -ErrorAction Ignore)
+    if ($anyFirewallsDisabled -or $adminRuleMissing) {
+      Write-Host "anyFirewallsDisabled: $anyFirewallsDisabled"
+      Write-Host "adminRuleMissing: $adminRuleMissing"
+      Write-Error "Failed to Set Firewall rule"
+    }
+  }
+}
+
+setup-firewall
 
 $env:PATH = "C:/var/vcap/bosh/bin;" + $env:PATH
 
