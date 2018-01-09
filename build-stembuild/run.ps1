04e7e6fa4321501637381ead232cf0788be13232
Jason Smith <jsmith@pivotal.io>
2018-01-09 17:44:53 -0500

    Fixing output log phrasing
    [#153703569](https://www.pivotaltracker.com/story/show/153703569)
    
    Signed-off-by: Ashwin Venkatesh <avenkatesh@pivotal.io>

--- build-stembuild/run.ps1
+++ build-stembuild/run.ps1
@@ -46,5 +46,5 @@ go test
 Write-Host ***Building Stembuild***
 go install
 
-Write-Host ***Copying stembuild binary to errand log directory***
+Write-Host ***Copying stembuild to output directory***
 copy $GO_DIR/bin/stembuild.exe $OUTPUT_DIR/stembuild-$VERSION.exe
