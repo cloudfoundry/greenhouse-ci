cfdc7812af2aaaaac452824dd38529c8e10cd6a0
Jason Smith <jsmith@pivotal.io>
2018-01-11 11:52:39 -0500

    remove garden files (moved to their ci repo)
    Signed-off-by: David Jahn <djahn@pivotal.io>

--- test-windows2016fs-windows/run.ps1
+++
@@ -1,9 +0,0 @@
-﻿$ErrorActionPreference = "Stop";
-trap { $host.SetShouldExit(1) }
-
-$env:GOPATH=$PWD
-
-go get github.com/onsi/ginkgo/ginkgo
-
-& "$env:GOPATH/bin/ginkgo.exe" -nodes $env:NODES -r -race -keepGoing -randomizeSuites src/code.cloudfoundry.org/windows2016fs
-Exit $LastExitCode
