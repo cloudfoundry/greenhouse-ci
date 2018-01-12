b8db88fa1f40137813dad41c263f5ca5255bcae5
Jason Smith <jsmith@pivotal.io>
2018-01-12 14:04:07 -0500

    Adding task to drop build number from stembuild binaries [#153495794]

---
+++ set-stembuild-version/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+RELEASE_VERSION=$( cat stembuild-version/version | cut -d '.' -f1-2 )
+BIN_DIRS=( stembuild-untested-windows stembuild-untested-linux )
+
+echo Releasing draft stembuild ${RELEASE_VERSION}
+
+for DIR in "${BIN_DIRS[@]}"
+do
+    PLATFORM=$( echo ${DIR} | cut -d '-' -f3)
+    echo *** Setting releasve version of ${PLATFORM} Stembuild ***
+    SOURCE_BINARY=$( find ${DIR} -name stembuild-* -type f -printf "%f\n" )
+    DEST_BINARY=$( echo $SOURCE_BINARY | sed -r 's/[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+/'${RELEASE_VERSION}'/g' )
+    cp ${DIR}/${SOURCE_BINARY} output/${DEST_BINARY}
+done
+
+echo ${RELEASE_VERSION} > output/tag
