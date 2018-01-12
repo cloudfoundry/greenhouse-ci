8ef827c0fac0a1c7c90af061ddfec06ef3eff2ac
Jason Smith <jsmith@pivotal.io>
2018-01-12 14:14:33 -0500

    Fixing script so it can run properly in concourse [#153495794]

--- set-stembuild-version/run.sh
+++ set-stembuild-version/run.sh
@@ -9,10 +9,10 @@ echo Releasing draft stembuild ${RELEASE_VERSION}
 
 for DIR in "${BIN_DIRS[@]}"
 do
-    PLATFORM=$( echo ${DIR} | cut -d '-' -f3)
-    echo *** Setting releasve version of ${PLATFORM} Stembuild ***
-    SOURCE_BINARY=$( find ${DIR} -name stembuild-* -type f -printf "%f\n" )
-    DEST_BINARY=$( echo $SOURCE_BINARY | sed -r 's/[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+/'${RELEASE_VERSION}'/g' )
+    PLATFORM=$( echo ${DIR} | cut -d '-' -f3 )
+    echo "*** Setting release version of ${PLATFORM} Stembuild ***"
+    SOURCE_BINARY=$( find ${DIR} -name stembuild-\* -type f -printf "%f\n" )
+    DEST_BINARY=$( echo ${SOURCE_BINARY} | sed -r 's/[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+/'${RELEASE_VERSION}'/g' )
     cp ${DIR}/${SOURCE_BINARY} output/${DEST_BINARY}
 done
 
