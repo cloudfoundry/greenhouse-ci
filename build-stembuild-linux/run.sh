b8cd1c235f262e5b0137a15cd0ce6d50546db0ca
Jason Smith <jsmith@pivotal.io>
2018-01-15 17:28:31 -0500

    Updating stembuild linux to use official librsync library
    [#152982588]

--- build-stembuild-linux/run.sh
+++ build-stembuild-linux/run.sh
@@ -14,12 +14,15 @@ echo ***Generating librsync sources***
 LIBRSYNC_DIR=${ROOT_DIR}/librsync
 LIBRSYNC_BUILD_DIR=${LIBRSYNC_DIR}/build
 LIBRSYNC_INSTALL_DIR=${LIBRSYNC_DIR}/install
+LIBRSYNC_BLAKE2_DIR=${LIBRSYNC_DIR}/src/blake2
+
 mkdir -p ${LIBRSYNC_BUILD_DIR}
 mkdir -p ${LIBRSYNC_INSTALL_DIR}
 
 pushd ${LIBRSYNC_BUILD_DIR}
   cmake -DCMAKE_INSTALL_PREFIX=${LIBRSYNC_INSTALL_DIR} -DCMAKE_BUILD_TYPE=release -G "Unix Makefiles" ..
   make
+  make test
 popd
 
 echo ***Copying librsync sources into stembuild***
@@ -29,6 +32,7 @@ STEMBUILD_RDIFF_DIR=${STEMBUILD_DIR}/rdiff
 pushd ${STEMBUILD_RDIFF_DIR}
   cp ${LIBRSYNC_DIR}/src/* .
   cp ${LIBRSYNC_BUILD_DIR}/src/* .
+  cp ${LIBRSYNC_BLAKE2_DIR}/* .
   rm rdiff.c
 popd
 
