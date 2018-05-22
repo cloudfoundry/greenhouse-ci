af7960c31d7422c8583ff24f137985bca4873a30
Stu Pollock <spollock@pivotal.io>
2018-05-22 14:35:11 -0400

    Force the bosh-psmodules task to fail when Pester reports a test failure
    [#156863607]https://www.pivotaltracker.com/story/show/156863607
    
    Signed-off-by: Michael Tang <mitang@pivotal.io>

--- tasks/bosh-windows-stemcell-builder/bosh-psmodules/run.ps1
+++ tasks/bosh-windows-stemcell-builder/bosh-psmodules/run.ps1
@@ -1,8 +1,13 @@
+$ErrorActionPreference = "Stop"
 # Do not set error action preference let Pester handle it instead
-
+$result = 0
 Import-Module ./stemcell-builder/src/github.com/pester/Pester/pester.psm1
 foreach ($module in (Get-ChildItem "./stemcell-builder/bosh-psmodules/modules").Name) {
   Push-Location "./stemcell-builder/bosh-psmodules/modules/$module"
     Invoke-Pester -EnableExit
+    if ($LASTEXITCODE -ne 0) {
+      $result = $LASTEXITCODE
+    }
   Pop-Location
 }
+exit $result
