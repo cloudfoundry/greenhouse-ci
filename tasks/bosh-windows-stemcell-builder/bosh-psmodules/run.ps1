47a08b2fd49cf6579f696c7e05bd9924b48a3045
bot <witchcraft@example.com>
2018-06-04 11:13:57 -0400

    Only fail on exit code greater than 0
    Signed-off-by: Stu Pollock <spollock@pivotal.io>

--- tasks/bosh-windows-stemcell-builder/bosh-psmodules/run.ps1
+++ tasks/bosh-windows-stemcell-builder/bosh-psmodules/run.ps1
@@ -1,13 +1,20 @@
 $ErrorActionPreference = "Stop"
 # Do not set error action preference let Pester handle it instead
 $result = 0
+
+$status = (Get-Service -Name "wuauserv").Status
+$startupType = Get-Service "wuauserv" | Select-Object -ExpandProperty StartType | Out-String
+"wuauserv status = * $status *"
+"wuauserv startuptype = * $startupType *"
+
 Import-Module ./stemcell-builder/src/github.com/pester/Pester/pester.psm1
 foreach ($module in (Get-ChildItem "./stemcell-builder/bosh-psmodules/modules").Name) {
   Push-Location "./stemcell-builder/bosh-psmodules/modules/$module"
     Invoke-Pester -EnableExit
-    if ($LASTEXITCODE -ne 0) {
-      $result = $LASTEXITCODE
+    if ($LASTEXITCODE -gt 0) {
+      $result += $LASTEXITCODE
     }
   Pop-Location
 }
+echo "Failed Tests: $result"
 exit $result
