fda185470e1d433cfdb6d15d447289eb8fa3faf6
Ashwin Venkatesh <avenkatesh@pivotal.io>
2018-08-14 15:44:08 -0400

    Add timestamp to zip file

--- tasks/bosh-windows-stemcell-automation/package/zip-files.bash
+++ tasks/bosh-windows-stemcell-automation/package/zip-files.bash
@@ -7,4 +7,4 @@ pushd stemcell-automation
 file_sources=`jq -r '.[] | select(.exclude_from_zip != true) | .file_source' $DIR/dependency_sources.json`
 cp $file_sources .
 zip StemcellAutomation.zip *
-cp StemcellAutomation.zip ../zip-file
+cp StemcellAutomation.zip ../zip-file/StemcellAutomation-`date +"%T"`.zip
