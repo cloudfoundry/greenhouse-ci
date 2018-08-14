8d4a0bc2cd4f60a46866d277747d93e6cfff95f2
Matthew Horan <mhoran@pivotal.io>
2018-08-14 14:15:39 -0400

    Add missing tasks from stemcell automation packaging
    Signed-off-by: Kenny DuMez <kdumez@pivotal.io>

---
+++ tasks/bosh-windows-stemcell-automation/package/zip-files.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+pushd stemcell-automation
+file_sources=`jq '.[] | select(.exclude_from_zip != true) | .file_source' $DIR/dependency_sources.json`
+cp $file_sources .
+zip StemcellAutomation.zip *
+cp StemcellAutomation.zip ../zip-file
