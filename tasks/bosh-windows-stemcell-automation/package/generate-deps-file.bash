bdec42a1a0a8a4aaf7a03995dd27fa586c812114
Ashwin Venkatesh <avenkatesh@pivotal.io>
2018-08-14 16:59:33 -0400

    Fixed generate-deps so that the deps file is present

--- tasks/bosh-windows-stemcell-automation/package/generate-deps-file.bash
+++ tasks/bosh-windows-stemcell-automation/package/generate-deps-file.bash
@@ -5,5 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cp ./lgpo/LGPO-*.zip ./lgpo/LGPO.zip
 pushd ./stemcell-automation
-ruby mk-deps.rb $DIR/dependency_sources.json | jq . > deps.json
-cat deps.json
+ruby mk-deps.rb $DIR/dependency_sources.json | jq . > ../deps-file/deps.json
