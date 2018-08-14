01cf916eb8103eecb2792504a74de19db8981852
Ashwin Venkatesh <avenkatesh@pivotal.io>
2018-08-14 14:34:04 -0400

    Fixed zip-files script

--- tasks/bosh-windows-stemcell-automation/package/zip-files.bash
+++ tasks/bosh-windows-stemcell-automation/package/zip-files.bash
@@ -4,7 +4,7 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 pushd stemcell-automation
-file_sources=`jq '.[] | select(.exclude_from_zip != true) | .file_source' $DIR/dependency_sources.json`
+file_sources=`jq -r '.[] | select(.exclude_from_zip != true) | .file_source' $DIR/dependency_sources.json`
 cp $file_sources .
 zip StemcellAutomation.zip *
 cp StemcellAutomation.zip ../zip-file
