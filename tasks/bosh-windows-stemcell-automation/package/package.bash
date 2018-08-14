503c6a4c9082694f7391b3bba211192eb3e2fb3f
Ashwin Venkatesh <avenkatesh@pivotal.io>
2018-08-14 14:11:25 -0400

    Added support for zipping dependency files

--- tasks/bosh-windows-stemcell-automation/package/package.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-
-cp ./lgpo/LGPO-*.zip ./lgpo/LGPO.zip
-pushd ./stemcell-automation
-ruby mk-deps.rb $DIR/dependency_sources.json | jq . > deps.json
-cat deps.json
