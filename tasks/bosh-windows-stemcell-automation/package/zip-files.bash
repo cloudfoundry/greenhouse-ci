d00b0a8b48ee4be3ca16b2e17c4027f91a41c8e4
Jason Smith <jsmith@pivotal.io>
2018-08-28 17:28:13 -0400

    Updating stemcell automation pipeline - Using a semver version to track version (no longer hardcoding a version) - Removing extra files from being packaged up

--- tasks/bosh-windows-stemcell-automation/package/zip-files.bash
+++ tasks/bosh-windows-stemcell-automation/package/zip-files.bash
@@ -6,5 +6,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 pushd stemcell-automation
 file_sources=`jq -r '.[] | select(.exclude_from_zip != true) | .file_source' $DIR/dependency_sources.json`
 cp $file_sources ../deps-file/deps.json .
-zip StemcellAutomation.zip *
+zip StemcellAutomation.zip * -x mk-deps.rb \*.Tests.\* NOTICE
 cp StemcellAutomation.zip ../zip-file/StemcellAutomation-`date +"%s"`.zip
