d0fe55c6bb256b4de428e3989cbeab42835a0c35
Simon Jones <sijones@pivotal.io>
2018-09-24 14:00:32 -0400

    Updated create-vsphere-stemcell-from-patchfile to be generic for OS version
    [#160270298](https://www.pivotaltracker.com/story/show/160270298)
    
    Signed-off-by: Malini Valliath <mvalliath@pivotal.io>

--- tasks/bosh-windows-stemcell-builder/create-vsphere-stemcell-from-patchfile/run.sh
+++ tasks/bosh-windows-stemcell-builder/create-vsphere-stemcell-from-patchfile/run.sh
@@ -2,7 +2,35 @@
 
 set -e
 
-VHD_VERSION=$(find 2012-vhd/*.vhd -printf "%f\n" | sed 's/en.2k12r2.serverdatacenter.rtm.fpp.patched-//g' | sed 's/.vhd//g')
+VHD_DIR="vhd"
+VHD_FILENAME=$(find ${VHD_DIR}/*.vhd -printf "%f")
+
+if [ -z "${VHD_FILENAME}" ]
+then
+  echo "Unable to find any vhd file in ${VHD_DIR}"
+  exit 1
+fi
+
+case "${OS_VERSION}" in
+
+"2012R2")
+  VHD_VERSION=$(echo "${VHD_FILENAME}" | sed 's/en.2k12r2.serverdatacenter.rtm.fpp.patched-//g' | sed 's/.vhd//g')
+  ;;
+1709|1803)
+  VHD_VERSION=$(echo "${VHD_FILENAME}" | sed -E 's/Windows-Server-Datacenter-Core-\d+-with-Containers-(.+)-en.*/\1/g')
+  ;;
+*)
+  echo "OS Version needs to be 2012R2, 1709, 1803, is: ${OS_VERSION}" >&2
+  exit 1
+  ;;
+esac
+
+if [ -z "${VHD_VERSION}" ]
+then
+  echo "Unable to extract vhd version from filename: ${VHD_FILENAME}, this is required" >&2
+  exit 1
+fi
+
 VERSION=$(cat version/version)
 
 echo "Installing OVFTool..."
@@ -24,14 +52,20 @@ echo "---" > $MANIFEST_FILE
 echo "patch_file: patchfile-$VERSION-$VHD_VERSION" >> $MANIFEST_FILE
 echo "os_version: $OS_VERSION" >> $MANIFEST_FILE
 echo "output_dir: $OUTPUT_DIR" >> $MANIFEST_FILE
-echo "vhd_file: $(ls 2012-vhd/*.vhd)" >> $MANIFEST_FILE
+echo "vhd_file: ${VHD_DIR}/${VHD_FILENAME}" >> $MANIFEST_FILE
 echo "version: $(cat version/version)" >> $MANIFEST_FILE
 echo "Using following patchfile: "
 cat $MANIFEST_FILE
 echo -e "\n\n"
 
+FILEPATH_OS_VERSION="${OS_VERSION}"
+if [ "${FILEPATH_OS_VERSION}" == "1709" ]
+then
+  FILEPATH_OS_VERSION="2016"
+fi
+
 echo "Building stemcell from patch file ..."
 stembuild/stembuild -output $OUTPUT_DIR apply-patch $MANIFEST_FILE
-mv $OUTPUT_DIR/*.tgz $OUTPUT_DIR/bosh-stemcell-$(cat version/version)-patch-vsphere-esxi-windows2012R2-go_agent.tgz
+mv $OUTPUT_DIR/*.tgz $OUTPUT_DIR/bosh-stemcell-$(cat version/version)-patch-vsphere-esxi-windows${FILEPATH_OS_VERSION}-go_agent.tgz
 mv $MANIFEST_FILE $OUTPUT_DIR
 echo "... done building stemcell from patch file"
