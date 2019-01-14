d367e5aeafc457538c45517479ff8954ababbf03
Jackson Feeny <jfeeny@pivotal.io>
2019-01-14 09:42:16 -0500

    Update Stembuild test jobs with missing information for deploying VM on vCenter
    Ensure OVA JSON variables are correctly filled in.
    
    [#161432730](https://www.pivotaltracker.com/story/show/161432730)
    
    Signed-off-by: Jason Smith <jsmith@pivotal.io>
    Co-authored-by: Jason Smith <jsmith@pivotal.io>

--- tasks/stembuild-linux-integration/run.sh
+++ tasks/stembuild-linux-integration/run.sh
@@ -6,7 +6,7 @@ VERSION=`cat stembuild-version/version`
 ROOT_DIR=$(cd `dirname "${BASH_SOURCE[0]}"`/../../.. && pwd)
 OUTPUT_DIR=${ROOT_DIR}/output
 
-JSON_TEMPLATE='{"DiskProvisioning": "thin","MarkAsTemplate": false,"Name": "stembuild_linux","IPAllocationPolicy": "fixedPolicy","IPProtocol": "IPv4","NetworkMapping": [{"Name": "custom","Network": "${VCENTER_WINNIPEG_NETWORK}"}],"PropertyMapping": [{"Key": "ip0","Value": "${EXISTING_VM_IP}"},{"Key": "cidr","Value": "25"},{"Key": "gateway","Value": "10.74.35.1"},{"Key": "DNS","Value": "8.8.8.8"}],"PowerOn": true,"InjectOvfEnv": false,"WaitForIP": false}'
+JSON_TEMPLATE='{"DiskProvisioning": "thin","MarkAsTemplate": false,"Name": "stembuild_linux","IPAllocationPolicy": "fixedPolicy","IPProtocol": "IPv4","NetworkMapping": [{"Name": "custom","Network": "'"${VCENTER_WINNIPEG_NETWORK}"'"}],"PropertyMapping": [{"Key": "ip0","Value": "'"${EXISTING_VM_IP}"'"},{"Key": "cidr","Value": "25"},{"Key": "gateway","Value": "10.74.35.1"},{"Key": "DNS","Value": "8.8.8.8"}],"PowerOn": true,"InjectOvfEnv": false,"WaitForIP": false}'
 
 echo ***Installing VMWare OVF Tools***
 chmod +x ./ovftool/VMware-ovftool-4.2.0-5965791-lin.x86_64.bundle
@@ -34,7 +34,7 @@ GO_STEMBUILD_DIR=${CF_INC_DIR}/stembuild
 pushd ${GO_STEMBUILD_DIR}
   echo ***Test Stembuild Code***
 
-  govc import.ova --options=<(echo ${JSON_TEMPLATE}) --name=stembuild_linux --folder=${VCENTER_VM_FOLDER} ../test.ova
+  govc import.ova --options=<(echo ${JSON_TEMPLATE}) --name=stembuild_linux --folder=${VCENTER_VM_FOLDER} ${ROOT_DIR}/test.ova
   make integration
   govc vm.destroy --vm.ip=${EXISTING_VM_IP}
 
