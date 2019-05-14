b60112734d8d51ec9c72c2f15f0b200df537e0a2
Malini Valliath <mvalliath@pivotal.io>
2019-05-14 15:54:18 -0400

    Add task to promote stembuild to Github
    - While modifying promote-stembuild job, we noticed that we weren't
    trimming the stembuild name before uploading to Pivnet. We re-added this
    task and added it to the promote-stembuild job
    - Add functionalility to "put" to Github release with appropriate tag and
    sha
    - Rename stembuild release/repository resources
    - Pivnet-upload task now takes in variable called "source-folder"
    instead of "file-to-upload"; more accurate variable name
    - OS_NAME was not used in pivnet-upload task, so removed as parameter
    and variable from vars folders
    
    [#165685860](https://www.pivotaltracker.com/story/show/165685860)
     As a product manager I want to distribute stembuild CLI to all users on Github in an automated way
    
    Co-authored-by: Simon Jones <sijones@pivotal.io>

---
+++ tasks/set-stembuild-version/run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -ex
+
+RELEASE_VERSION=$( cat stembuild-version/version | cut -d '.' -f1-2 )
+BIN_DIRS=( stembuild-untested-windows stembuild-untested-linux )
+
+for DIR in "${BIN_DIRS[@]}"
+do
+    PLATFORM=$( echo ${DIR} | cut -d '-' -f3 )
+    echo "*** Setting release version of ${PLATFORM} Stembuild ***"
+    SOURCE_BINARY=$( find ${DIR} -name stembuild-\* -type f -printf "%f\n" )
+    DEST_BINARY=$( echo ${SOURCE_BINARY} | sed -r 's/[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+/'${RELEASE_VERSION}'/g' )
+    cp ${DIR}/${SOURCE_BINARY} final-stembuilds/${DEST_BINARY}
+done
+
+echo ${RELEASE_VERSION} > final-stembuilds/tag
