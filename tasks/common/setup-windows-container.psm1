f657eebb327e1c8dd6f50441635292a7cc3db80e
Gab Satchi <gsatchithanantham@pivotal.io>
2019-05-29 16:28:28 -0400

    Adds required ItemType field

--- tasks/common/setup-windows-container.psm1
+++ tasks/common/setup-windows-container.psm1
@@ -3,6 +3,6 @@ function Set-TmpDir
     $working_directory = Get-Location
     $guid = $( New-Guid ).Guid
     $temp_directory=Join-Path -Path $working_directory.path -ChildPath "temp-$guid"
-    New-Item -Path $temp_directory
+    New-Item -Path $temp_directory -ItemType Directory
     $env:TMP=$temp_directory
 }
