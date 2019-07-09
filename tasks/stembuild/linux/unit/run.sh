a70f7afe6223c63b7393767bcc5c8e420e64c6c1
Jackson Feeny <jfeeny@pivotal.io>
2019-07-09 14:15:51 -0400

    Get vcsim in stembuild-linux task
    [#166802379](https://www.pivotaltracker.com/story/show/166802379)
     Write a new vcenter_client package that uses govmomi instead of govc [START, WAITFOREXIT]
    
    Co-authored-by: Tal Usvyatsky <tusvyatsky@pivotal.io>

--- tasks/stembuild/linux/unit/run.sh
+++ tasks/stembuild/linux/unit/run.sh
@@ -19,8 +19,9 @@ STEMBUILD_DIR=${ROOT_DIR}/stembuild
 mkdir -p ${CF_INC_DIR}
 cp -r ${STEMBUILD_DIR} ${CF_INC_DIR}
 
-# install ginkgo, govc
+# install ginkgo, govc, vcsim
 go get github.com/onsi/ginkgo/ginkgo
+go get -u github.com/vmware/govmomi/vcsim
 
 GO_STEMBUILD_DIR=${CF_INC_DIR}/stembuild
 pushd ${GO_STEMBUILD_DIR}
