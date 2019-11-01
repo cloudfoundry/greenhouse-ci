e30527888fbe0e3efa301a3b02412d713a161f3d
Daniel Basner <dbasner@pivotal.io>
2019-11-01 10:06:07 -0400

    Modify create-vsphere to use bosh-agent release resource
    [#168794582](https://www.pivotaltracker.com/story/show/168794582)
     Make BOSH agent resource pull from release bucket rather than master branch

--- tasks/bosh-windows-stemcell-builder/create-vsphere-stemcell-from-vmx/run.ps1
+++ tasks/bosh-windows-stemcell-builder/create-vsphere-stemcell-from-vmx/run.ps1
@@ -9,6 +9,11 @@ mv "${PWD}\stembuild\stembuild-windows-x86_64-*.exe" "${PWD}\bin\stembuild.exe"
 
 $env:PATH="${PWD}\bin;$env:PATH"
 
+$env:BOSH_AGENT_DIR="$(pwd)/$BOSH_AGENT_DIR"
+pushd "${BOSH_AGENT_DIR}"
+  mv bosh-agent*.exe bosh-agent.exe
+popd
+
 pushd "stemcell-builder"
 bundle install --without test
 if ($LASTEXITCODE -ne 0) {
