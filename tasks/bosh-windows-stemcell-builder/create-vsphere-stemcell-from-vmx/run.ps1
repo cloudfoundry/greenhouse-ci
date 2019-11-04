fb47300903f19547f84390775d2d4e9fdd5846e3
Daniel Basner <dbasner@pivotal.io>
2019-11-04 11:50:44 -0500

    Store bosh agent release directory in an environment variable
    [#168794582](https://www.pivotaltracker.com/story/show/168794582)
     Make BOSH agent resource pull from release bucket rather than master branch

--- tasks/bosh-windows-stemcell-builder/create-vsphere-stemcell-from-vmx/run.ps1
+++ tasks/bosh-windows-stemcell-builder/create-vsphere-stemcell-from-vmx/run.ps1
@@ -9,8 +9,8 @@ mv "${PWD}\stembuild\stembuild-windows-x86_64-*.exe" "${PWD}\bin\stembuild.exe"
 
 $env:PATH="${PWD}\bin;$env:PATH"
 
-$BOSH_AGENT_DIR="$(pwd)/$env:BOSH_AGENT_DIR"
-pushd "${BOSH_AGENT_DIR}"
+$env:BOSH_AGENT_DIR="$(pwd)/$env:BOSH_AGENT_DIR"
+pushd "${env:BOSH_AGENT_DIR}"
   mv bosh-agent*.exe bosh-agent.exe
 popd
 
