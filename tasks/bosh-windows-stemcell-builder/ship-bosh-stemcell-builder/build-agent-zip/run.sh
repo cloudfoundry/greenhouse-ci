88c69f31a01d3ad73c90583d913eb8ff4b63ef07
George Gelashvili <ggelashvili+cf@pivotal.io>
2019-11-05 11:42:09 -0500

    Checkout bosh-agent source that corresponds to compiled bosh-agent
    - We consume some dependencies from bosh-agent source
    - This change ensures that the dependencies we consume are the same as what
    is used in the compiled bosh-agent
    
    [#168794582](https://www.pivotaltracker.com/story/show/168794582)
     Make BOSH agent resource pull from release bucket rather than master branch

--- tasks/bosh-windows-stemcell-builder/ship-bosh-stemcell-builder/build-agent-zip/run.sh
+++ tasks/bosh-windows-stemcell-builder/ship-bosh-stemcell-builder/build-agent-zip/run.sh
@@ -5,10 +5,14 @@ set -ex
 export BOSH_AGENT_DIR="$(pwd)/$BOSH_AGENT_DIR"
 
 pushd "${BOSH_AGENT_DIR}"
+  CURRENT_VERSION=$(cat .resource/version)
   mv bosh-agent*.exe bosh-agent.exe
 popd
 
 pushd stemcell-builder
+  pushd src/github.com/cloudfoundry/bosh-agent
+    git checkout "v$CURRENT_VERSION"
+  popd
   bundle
   rake package:agent
   mv build/*.zip ../bosh-agent
