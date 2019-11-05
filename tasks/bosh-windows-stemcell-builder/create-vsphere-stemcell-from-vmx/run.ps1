fb50f7eea614b04037d64e37e8f66c6b25ff37a0
Daniel Basner <dbasner@pivotal.io>
2019-11-05 14:28:09 -0500

    Checkout bosh-agent source associated with release for all IaaSes
    [#168794582](https://www.pivotaltracker.com/story/show/168794582)
     Make BOSH agent resource pull from release bucket rather than master branch

--- tasks/bosh-windows-stemcell-builder/create-vsphere-stemcell-from-vmx/run.ps1
+++ tasks/bosh-windows-stemcell-builder/create-vsphere-stemcell-from-vmx/run.ps1
@@ -11,30 +11,36 @@ $env:PATH="${PWD}\bin;$env:PATH"
 
 $env:BOSH_AGENT_DIR="$(pwd)/$env:BOSH_AGENT_DIR"
 pushd "${env:BOSH_AGENT_DIR}"
+  $CURRENT_VERSION=$(Get-Content .resource/version)
   mv bosh-agent*.exe bosh-agent.exe
 popd
 
 pushd "stemcell-builder"
 bundle install --without test
-if ($LASTEXITCODE -ne 0) {
-  Write-Error "Could not bundle install"
-  Exit 1
-}
-rake package:agent
-if ($LASTEXITCODE -ne 0) {
-  Write-Error "Could not package agent"
-  Exit 1
-}
-rake package:psmodules
-if ($LASTEXITCODE -ne 0) {
-  Write-Error "Could not package psmodules"
-  Exit 1
-}
-rake build:vsphere
-if ($LASTEXITCODE -ne 0) {
-  Write-Error "Could not build vsphere"
-  Exit 1
-}
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not bundle install"
+    Exit 1
+  }
+  pushd src/github.com/cloudfoundry/bosh-agent
+    git checkout "v$CURRENT_VERSION"
+  popd
 
+  rake package:agent
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not package agent"
+    Exit 1
+  }
+
+  rake package:psmodules
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not package psmodules"
+    Exit 1
+  }
+
+  rake build:vsphere
+  if ($LASTEXITCODE -ne 0) {
+    Write-Error "Could not build vsphere"
+    Exit 1
+  }
 popd
 mv stemcell-builder/hotfixes.log hotfix-log/
