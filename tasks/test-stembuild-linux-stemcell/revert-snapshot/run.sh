2c00827ce8206af6574c61cc8276e7007c23ae47
Sam Coward <scoward@pivotal.io>
2019-11-26 15:42:08 -0500

    VM still needs powering on as restoring powered on snapshot yields VM in suspended state
    [#169151708]
    
    Co-authored-by: Daniel Basner <dbasner@pivotal.io>

--- tasks/test-stembuild-linux-stemcell/revert-snapshot/run.sh
+++ tasks/test-stembuild-linux-stemcell/revert-snapshot/run.sh
@@ -2,3 +2,4 @@
 set -exu
 
 govc snapshot.revert -u=${CREDENTIAL_URL} -s=true -dc=${DATACENTER} -vm=${VM_TO_REVERT} "${SNAPSHOT_NAME}"
+govc vm.power -u=${CREDENTIAL_URL} -dc=${DATACENTER} -on=true ${VM_TO_REVERT}
