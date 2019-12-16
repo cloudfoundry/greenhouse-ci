40ae3745115a18132a12a6270cee232c108725db
Flora Gallina-Jones <fgallinajones@pivotal.io>
2019-12-16 13:59:03 -0500

    remove psmodules rake from create-vmx-add-updates's
    run script, because we suspect that the script doesn't
    actually depend on psmodules
    
    [#168997593](https://www.pivotaltracker.com/story/show/168997593)
     Extract BOSH.PSModules out to its own repo

--- tasks/bosh-windows-stemcell-builder/create-vmx-add-updates/run.ps1
+++ tasks/bosh-windows-stemcell-builder/create-vmx-add-updates/run.ps1
@@ -9,11 +9,6 @@ if ($LASTEXITCODE -ne 0) {
   Write-Error "Could not bundle install"
   Exit 1
 }
-rake package:psmodules
-if ($LASTEXITCODE -ne 0) {
-  Write-Error "Could not package psmodules"
-  Exit 1
-}
 rake build:vsphere_add_updates
 if ($LASTEXITCODE -ne 0) {
   Write-Error "Could not install updates to vsphere image"
