ff214461995d497a02e9db8f75f90a8bbf6405ec
Flora Gallina-Jones <fgallinajones@pivotal.io>
2019-12-17 09:47:31 -0500

    restored psmodules package command to create-vmx-add-updates
    to fix [this build failure](https://bosh-windows.ci.cf-app.com/teams/main/pipelines/utilities/jobs/create-vsphere-add-updates/builds/35)
    
    [#168997593](https://www.pivotaltracker.com/story/show/168997593)
     Extract BOSH.PSModules out to its own repo
    
    Co-authored-by: Tal Usvyatsky <tusvyatsky@pivotal.io>

--- tasks/bosh-windows-stemcell-builder/create-vmx-add-updates/run.ps1
+++ tasks/bosh-windows-stemcell-builder/create-vmx-add-updates/run.ps1
@@ -9,6 +9,11 @@ if ($LASTEXITCODE -ne 0) {
   Write-Error "Could not bundle install"
   Exit 1
 }
+rake package:psmodules
+if ($LASTEXITCODE -ne 0) {
+  Write-Error "Could not package psmodules"
+  Exit 1
+}
 rake build:vsphere_add_updates
 if ($LASTEXITCODE -ne 0) {
   Write-Error "Could not install updates to vsphere image"
