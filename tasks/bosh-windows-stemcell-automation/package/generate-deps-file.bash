ed920486572d115cb281801d311bd1a23ce1fd3a
Flora Gallina-Jones <fgallinajones@pivotal.io>
2020-02-12 18:24:54 -0500

    removed dependencies on separate stemcell-automation repo
    so that CI can build when stemcell-automation lives inside stembuild.
    Also removed the need for a dependency_sources.json
    
    [#168997631](https://www.pivotaltracker.com/story/show/168997631)
     move stemcell automation to stembuild
    
    Co-authored-by: Sam Coward <scoward@pivotal.io>
    Co-authored-by: Austina Lin <aulin@pivotal.io>
    Co-authored-by: Sam Coward <scoward@pivotal.io>

--- tasks/bosh-windows-stemcell-automation/package/generate-deps-file.bash
+++ tasks/bosh-windows-stemcell-automation/package/generate-deps-file.bash
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 cp ./lgpo/LGPO-*.zip ./lgpo/LGPO.zip
-pushd ./stemcell-automation
-ruby mk-deps.rb $DIR/dependency_sources.json | jq . > ../deps-file/deps.json
+
+cat <<EOF > deps-file/deps.json
+{
+  "OpenSSH-Win64.zip": {
+    "sha": "$(shasum -a 256 open-ssh/OpenSSH-Win64.zip | cut -d " " -f 1)",
+    "version": "$(cat open-ssh/version)"
+  },
+  "bosh-psmodules.zip": {
+    "sha": "$(shasum -a 256 bosh-psmodules/bosh-psmodules.zip | cut -d " " -f 1)",
+    "version": "$(cat version/version)"
+  },
+  "agent.zip": {
+    "sha": "$(shasum -a 256 bosh-agent/agent.zip | cut -d " " -f 1)",
+    "version": "$(cat version/version)"
+  },
+  "LGPO.zip": {
+    "sha": "$(shasum -a 256 lgpo/LGPO.zip | cut -d " " -f 1)",
+    "version": "$(cat lgpo/version)"
+  }
+}
+EOF
