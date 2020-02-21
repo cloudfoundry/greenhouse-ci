ed4ab3364a405f756853a6ded90c37773350cc1c
Flora Gallina-Jones <fgallinajones@pivotal.io>
2020-02-21 12:31:34 -0500

    create-stembuild-linux-stemcell now uses patch version flag
    of stembuild package, so output file includes patch and build numbers
    
    Co-authored-by: Austina Lin <aulin@pivotal.io>

--- tasks/test-stembuild-linux-stemcell/stembuild-package/run.sh
+++ tasks/test-stembuild-linux-stemcell/stembuild-package/run.sh
@@ -9,8 +9,12 @@ mv stembuild-untested-linux/stembuild .
 
 chmod 500 stembuild
 
+version="$(cat build-number/number)"
+IFS='.' read -r -a array <<< "$version"
+patch_version="${array[2]}.${array[3]}"
+
 ./stembuild package \
-  -vcenter-url ${VCENTER_BASE_URL} -vcenter-username ${VCENTER_USERNAME} -vcenter-password ${VCENTER_PASSWORD} -vm-inventory-path ${VCENTER_VM_FOLDER}/${STEMBUILD_BASE_VM_NAME}
+  -vcenter-url ${VCENTER_BASE_URL} -vcenter-username ${VCENTER_USERNAME} -vcenter-password ${VCENTER_PASSWORD} -vm-inventory-path ${VCENTER_VM_FOLDER}/${STEMBUILD_BASE_VM_NAME} -patch-version ${patch_version}
 
 mv *.tgz stembuild-built-stemcell
 
