cd9f1fd3e119e7b866c8f337920936464f97c7ad
George Gelashvili <ggelashvili+cf@pivotal.io>
2020-03-05 15:58:51 -0500

    Recreate network interface as part of reverting target vm state
    before we run windows updates or stembuild construct, in our
    stembuild-*-stemcell jobs
    
    this is necessary because stembuild package removes the network device,
    so it is then missing on subsequent builds. leading thinking is that the
    latest vmware tools update prevents snapshot restores from correctly
    restoring the network device when the VM is reverted to an earlier state
    
    [#168996632](https://www.pivotaltracker.com/story/show/168996632)
     As an internal platform engineer I want to have confidence in stemcells built using stembuild as compared to public IaaS Windows stemcells
    
    Co-authored-by: Daniel Basner <dbasner@pivotal.io>

--- tasks/test-stembuild-linux-stemcell/revert-snapshot/run.sh
+++ tasks/test-stembuild-linux-stemcell/revert-snapshot/run.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
-set -exu
+set -eu
 
+#check if there is a network device attached, if not, add, maybe check power state too
+echo "Retrieving device info to ensure vm has networking"
+set +e
+govc device.info  -u ${CREDENTIAL_URL} -vm.ipath=${VM_TO_REVERT} 'ethernet-*'
+ethernetPresent=$?
+set -e
+if [[ ${ethernetPresent} -ne 0 ]] ; then
+    set -e
+    echo "No network device present, adding network device to VM"
+    govc vm.network.add  -u ${CREDENTIAL_URL} -vm.ipath=${VM_TO_REVERT} -net="calgary" -net.adapter="e1000e"
+fi
+set -e
+
+echo "Reverting ${VM_TO_REVERT} to snapshot ${SNAPSHOT_NAME}"
 govc snapshot.revert -u=${CREDENTIAL_URL} -s=true -dc=${DATACENTER} -vm=${VM_TO_REVERT} "${SNAPSHOT_NAME}"
 govc vm.power -u=${CREDENTIAL_URL} -dc=${DATACENTER} -on=true ${VM_TO_REVERT}
