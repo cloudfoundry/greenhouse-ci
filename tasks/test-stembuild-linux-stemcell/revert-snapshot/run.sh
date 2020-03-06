88726bf584a502c75517e10be1bbdd0583c70898
George Gelashvili <ggelashvili+cf@pivotal.io>
2020-03-06 12:20:43 -0500

    Simplify testing result of existing network adapter
    [#168996632](https://www.pivotaltracker.com/story/show/168996632)
     As an internal platform engineer I want to have confidence in stemcells built using stembuild as compared to public IaaS Windows stemcells
    
    Co-authored-by: Daniel Basner <dbasner@pivotal.io>

--- tasks/test-stembuild-linux-stemcell/revert-snapshot/run.sh
+++ tasks/test-stembuild-linux-stemcell/revert-snapshot/run.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 set -eu
 
-#check if there is a network device attached, if not, add, maybe check power state too
 echo "Retrieving device info to ensure vm has networking"
-set +e
-govc device.info  -u ${CREDENTIAL_URL} -vm.ipath=${VM_TO_REVERT} 'ethernet-*'
-ethernetPresent=$?
-set -e
-if [[ ${ethernetPresent} -ne 0 ]] ; then
-    set -e
+if ! govc device.info  -u ${CREDENTIAL_URL} -vm.ipath=${VM_TO_REVERT} 'ethernet-*' ; then
     echo "No network device present, adding network device to VM"
     govc vm.network.add  -u ${CREDENTIAL_URL} -vm.ipath=${VM_TO_REVERT} -net="calgary" -net.adapter="e1000e"
+    govc device.info  -u ${CREDENTIAL_URL} -vm.ipath=${VM_TO_REVERT} 'ethernet-*'
 fi
-set -e
 
 echo "Reverting ${VM_TO_REVERT} to snapshot ${SNAPSHOT_NAME}"
 govc snapshot.revert -u=${CREDENTIAL_URL} -s=true -dc=${DATACENTER} -vm=${VM_TO_REVERT} "${SNAPSHOT_NAME}"
