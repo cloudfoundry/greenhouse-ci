816de1b6374481cadbb88ee080673be92b25c084
Flora Gallina-Jones <fgallinajones@pivotal.io>
2020-05-01 10:28:42 -0400

    flattene CI task directory structure
    and rename some tasks. so that our CI is easier to navigate and
    understand

---
+++ tasks/test-units-stembuild-windows/run.ps1
@@ -0,0 +1,32 @@
+$ErrorActionPreference = "Stop";
+trap { Exit 1 }
+
+Import-Module ./ci/common-scripts/setup-windows-container.psm1
+Set-TmpDir
+
+$ROOT_DIR= (Get-Item "$PSScriptRoot/../../../../..").FullName
+
+$GO_DIR=Join-Path $ROOT_DIR go-work
+$STEMBUILD_DIR="$GO_DIR/src/github.com/cloudfoundry-incubator/stembuild"
+
+$env:GOPATH = $GO_DIR
+Write-Host "GOPATH: $env:GOPATH"
+
+New-Item $GO_DIR -ItemType Directory
+
+Write-Host ***Cloning stembuild***
+Copy-Item stembuild $STEMBUILD_DIR -Recurse -Force
+
+$env:PATH="${GO_DIR}/bin;$env:PATH"
+
+Write-Host ***Building ginkgo***
+go get github.com/onsi/ginkgo/ginkgo
+go install github.com/onsi/ginkgo/ginkgo
+
+Write-Host ***Test Stembuild Code***
+Set-Location $STEMBUILD_DIR
+make units
+if ($lastexitcode -ne 0)
+{
+  throw "unit tests failed"
+}
