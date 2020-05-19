56964a9ef879edf44d0971dcb19c8c094075dc27
Austina Lin <aulin@pivotal.io>
2020-05-19 18:07:44 -0400

    add the necessary steps to bbl up
    [#172681675](https://www.pivotaltracker.com/story/show/172681675)
     Create a new concourse pipeline that can spin up a new bosh environment
    
    Co-authored-by: Anand Gaitonde <agaitonde@pivotal.io>

--- tasks/run-bbl/run.sh
+++ tasks/run-bbl/run.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-set -eu
+set -eEux
 
-env
+export WORKSPACE_DIR=$(pwd)
+
+function commit(){
+    git add .
+    git commit -m "${BBL_ENV_NAME}: ${GIT_COMMIT_MESSAGE}"
+    rsync -a "${WORKSPACE_DIR}/greenhouse-private/ ${WORKSPACE_DIR}/updated-greenhouse-private # / at the end of private is required
+}
+
+git config user.name "${GIT_COMMIT_USERNAME}"
+git config user.email "${GIT_COMMIT_EMAIL}"
+
+mkdir -p "greenhouse-private/dev-envs/${BBL_ENV_NAME}"
+pushd "greenhouse-private/dev-envs/${BBL_ENV_NAME}"
+    bbl plan
+
+    ln -sf ../../scripts/create-director-override-gcp.sh create-director-override.sh
+
+    rm -rf bosh-deployment jumpbox-deployment
+    cp -a "${WORKSPACE_DIR}/bosh-deployment" "${WORKSPACE_DIR}/jumpbox-deployment" .
+
+    trap commit ERR
+    bbl up
+
+    commit
+popd
