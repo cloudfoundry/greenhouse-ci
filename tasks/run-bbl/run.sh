8a04bff8ff717e2639385b2d430ba66fcd1c7648
Anand Gaitonde <agaitonde@pivotal.io>
2020-05-20 11:47:48 -0400

    Created our own bbl setup task
    - Only bbls up a GCP env with an internal blobstore
    - cp has weird issues copying directories (for some reason it's copying
      it's parent directories in addition to it's sub directories) so we're
      installing rsync and using that instead
    - configure username / email globally so we can setup git up front
      instead of inside each git repo
    - Automate environment with latest bosh-deployment & jumpbox-deployment
    
    Co-authored-by: Flora Gallina-Jones <fgallinajones@pivotal.io>
    Co-authored-by: Anand Gaitonde <agaitonde@pivotal.io>
    Co-authored-by: George Gelashvili <ggelashvili+cf@pivotal.io>
    Co-authored-by: Austina Lin <aulin@pivotal.io>
    Co-authored-by: Sam Coward <scoward@pivotal.io>
    
    [#172681675](https://www.pivotaltracker.com/story/show/172681675)
     Create a new concourse pipeline that can spin up a new bosh environment

---
+++ tasks/run-bbl/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eEux
+
+export WORKSPACE_DIR=$(pwd)
+apt install rsync -y
+
+function commit(){
+    git add .
+    git commit -m "${BBL_ENV_NAME}: ${GIT_COMMIT_MESSAGE}"
+    rsync -a "${WORKSPACE_DIR}/greenhouse-private/" "${WORKSPACE_DIR}/updated-greenhouse-private" # / at the end of private is required
+}
+
+git config --global user.name "${GIT_COMMIT_USERNAME}"
+git config --global user.email "${GIT_COMMIT_EMAIL}"
+
+mkdir -p "greenhouse-private/dev-envs/${BBL_ENV_NAME}"
+pushd "greenhouse-private/dev-envs/${BBL_ENV_NAME}"
+    bbl plan
+
+    ln -sf ../../scripts/create-director-override-gcp.sh create-director-override.sh
+
+    rm -rf bosh-deployment jumpbox-deployment
+    cp -a "${WORKSPACE_DIR}/bosh-deployment" "${WORKSPACE_DIR}/jumpbox-deployment" .
+
+    trap commit ERR
+    bbl up
+
+    commit
+popd
