ad53096f982adf951452d7f6410fe9919ca80bf5
Flora Gallina-Jones <fgallinajones@pivotal.io>
2020-06-01 14:54:15 -0400

    WIP adding pipeline config for more iaases

--- tasks/run-bbl/run.sh
+++ tasks/run-bbl/run.sh
@@ -11,17 +11,36 @@ function commit(){
     rsync -a "${WORKSPACE_DIR}/greenhouse-private/" "${WORKSPACE_DIR}/updated-greenhouse-private" # / at the end of private is required
 }
 
+
+function loadSecrets() {
+  case "${BBL_IAAS}" in
+  gcp)
+  echo "${SWAN_ACCOUNT_JSON}" > gcp-service-account-key.json
+  export BBL_GCP_SERVICE_ACCOUNT_KEY="${PWD}/gcp-service-account-key.json"
+  ;;
+  aws)
+  echo ${BBL_AWS_ACCESS_KEY_ID} > aws-access-key-id
+#  echo ${BBL_AWS_SECRET_ACCESS_KEY} > aws-secret-access-key
+  ;;
+  azure)
+#  echo ${AZURE_DEV_JSON} > azure-client-credentials.json
+  echo ${BBL_AZURE_SUBSCRIPTION_ID} > azure-dev-subscription-id
+  echo ${BBL_AZURE_TENANT_ID} > azure-dev-tenant-id
+  echo ${BBL_AZURE_CLIENT_ID} > azure-dev-client-id
+  ;;
+  esac
+}
+
 git config --global user.name "${GIT_COMMIT_USERNAME}"
 git config --global user.email "${GIT_COMMIT_EMAIL}"
 
 mkdir -p "greenhouse-private/dev-envs/${BBL_ENV_NAME}"
 pushd "greenhouse-private/dev-envs/${BBL_ENV_NAME}"
-    echo "${SWAN_ACCOUNT_JSON}" > gcp-service-account-key.json
-    export BBL_GCP_SERVICE_ACCOUNT_KEY="${PWD}/gcp-service-account-key.json"
+    loadSecrets
 
     bbl plan
 
-    ln -sf ../../scripts/create-director-override-gcp.sh create-director-override.sh
+    ln -sf ../../scripts/create-director-override-${BBL_IAAS}.sh create-director-override.sh
 
     rm -rf bosh-deployment jumpbox-deployment
     cp -a "${WORKSPACE_DIR}/bosh-deployment" "${WORKSPACE_DIR}/jumpbox-deployment" .
