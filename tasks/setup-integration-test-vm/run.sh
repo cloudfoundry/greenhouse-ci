ae56c9b3021d32d1a38b9f7d50fc03b0f5676fc2
Flora Gallina-Jones <fgallinajones@pivotal.io>
2020-06-26 16:06:28 -0400

    setup-integration-test-vm now clones VM into a folder
    so that it's easier to find
    
    [#173494537](https://www.pivotaltracker.com/story/show/173494537)
     Update \Create base image" docs to use Administrator user, not use VMware Fusion, follow public docs more"
    
    Co-authored-by: Austina Lin <aulin@pivotal.io>

--- tasks/setup-integration-test-vm/run.sh
+++ tasks/setup-integration-test-vm/run.sh
@@ -2,25 +2,25 @@
 
 set -ex
 
-export VM_NAME_PREFIX="construct-linux-integration-ci-${OS_LINE}"
+export CLONE_NAME_PREFIX="construct-linux-integration-ci-${OS_LINE}"
 
 ROOT_DIR=$(pwd)
 OUTPUT_DIR=${ROOT_DIR}/output
 
 export VM_IP=`cat vcenter-ips/name`
-export VM_NAME_SUFFIX=$(echo ${VM_IP} | cut -d . -f 4)
-export VM_NAME=${VM_NAME_PREFIX}${VM_NAME_SUFFIX}
-echo "Creating VM ${VM_NAME} with IP: ${VM_IP}"
+export CLONE_NAME_SUFFIX=$(echo ${VM_IP} | cut -d . -f 4)
+export CLONE_NAME=${CLONE_NAME_PREFIX}${CLONE_NAME_SUFFIX}
+echo "Creating VM ${CLONE_NAME} with IP: ${VM_IP}"
 
-govc vm.clone -u ${VCENTER_ADMIN_CREDENTIAL_URL} -vm ${BASE_VM_IPATH} -ds ${CLONE_DATASTORE} -pool ${CLONE_RESOURCE_POOL} -on=false $VM_NAME
-govc vm.customize -u ${VCENTER_ADMIN_CREDENTIAL_URL} -vm.ipath /pizza-boxes-dc/vm/${VM_NAME} -ip ${VM_IP} ${VM_CUSTOMIZATION_NAME}
-govc vm.power -on -u ${VCENTER_ADMIN_CREDENTIAL_URL} -vm.ipath /pizza-boxes-dc/vm/${VM_NAME}
+govc vm.clone -u ${VCENTER_ADMIN_CREDENTIAL_URL} -vm ${BASE_VM_IPATH} -ds ${CLONE_DATASTORE} -pool ${CLONE_RESOURCE_POOL} -folder "$CLONE_FOLDER" -on=false "$CLONE_NAME"
+govc vm.customize -u ${VCENTER_ADMIN_CREDENTIAL_URL} -vm.ipath "$CLONE_FOLDER"/"$CLONE_NAME" -ip ${VM_IP} ${VM_CUSTOMIZATION_NAME}
+govc vm.power -on -u ${VCENTER_ADMIN_CREDENTIAL_URL} -vm.ipath "$CLONE_FOLDER"/"$CLONE_NAME"
 
 echo Waiting for VM to be configured with expected IP address...
 SECONDS=0
 while [ "$VM_IP" != "$CURRENT_IP_ADDRESS" ]; do
 	sleep 10
-	CURRENT_IP_ADDRESS=$(govc vm.info -u "$VCENTER_ADMIN_CREDENTIAL_URL" -json /pizza-boxes-dc/vm/"$VM_NAME" | jq -r ".VirtualMachines[0].Guest.IpAddress")
+	CURRENT_IP_ADDRESS=$(govc vm.info -u "$VCENTER_ADMIN_CREDENTIAL_URL" -json "$CLONE_FOLDER"/"$CLONE_NAME" | jq -r ".VirtualMachines[0].Guest.IpAddress")
 	echo Current IP Address is "$CURRENT_IP_ADDRESS"
 	if [ $SECONDS -gt 600 ] ; then
 		exit 1
