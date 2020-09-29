877501870635fcf4c2641cf4e185a2f85ede75b6
Sam Coward <scoward@pivotal.io>
2020-09-29 19:53:43 -0400

    wip

--- tasks/run-construct-stembuild-linux/run.sh
+++ tasks/run-construct-stembuild-linux/run.sh
@@ -4,7 +4,7 @@ set -ex
 cat > ca.crt <<END_OF_CERT
 $VCENTER_CA_CERT
 END_OF_CERT
-export GOVC_TLS_CA_CERTS=ca.crt
+#export GOVC_TLS_CA_CERTS=ca.crt
 
 pushd stembuild-untested-linux
   mv stembuild* stembuild
@@ -15,6 +15,7 @@ mv lgpo-binary/LGPO*.zip LGPO.zip
 
 chmod 500 stembuild
 ./stembuild construct \
+  -vcenter-ca-certs ca.crt \
   -vcenter-url ${VCENTER_BASE_URL} -vcenter-username ${VCENTER_USERNAME} -vcenter-password ${VCENTER_PASSWORD} \
   -vm-inventory-path ${VCENTER_VM_FOLDER}/${STEMBUILD_BASE_VM_NAME} \
   -vm-ip ${STEMBUILD_BASE_VM_IP} -vm-username ${STEMBUILD_BASE_VM_USERNAME} -vm-password ${STEMBUILD_BASE_VM_PASSWORD}
