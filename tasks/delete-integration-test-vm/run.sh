283c5c82e0c49849d8bfd73c73451e2e4d6b1309
Anand Gaitonde <agaitonde@pivotal.io>
2020-11-09 14:42:41 -0800

    make integration-vm-name optional input for deletion
    - this is so that if the setup task fails, the deletion task will not
    prevent subsequent steps in the 'ensure:' from running.
    
    Co-authored-by: George Gelashvili <ggelashvili+cf@pivotal.io>

--- tasks/delete-integration-test-vm/run.sh
+++ tasks/delete-integration-test-vm/run.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-set -ux # Do not set 'e' as govc will fail if the vm has already been cleaned up
+set -u # Do not set 'e' as govc will fail if the vm has already been cleaned up
+
+if [[ ! -d integration-vm-name ]]; then
+  echo "integration vm information not provided as an input"
+  echo "continuing (by doing nothing), so we clear IP locks in the next step"
+  echo "please see your previous tasks if this is unexpected"
+  exit 0
+fi
 
 export VM_NAME=$(cat integration-vm-name/name)
 export VM_IPATH="${CLONE_FOLDER}/${VM_NAME}"
