f8852f5d596474903a1e1c604d62721ec2c263bf
Sam Coward <scoward@pivotal.io>
2021-02-11 10:11:47 -0500

    Install PSWindowsUpdate on base image via NuGet
    [#176643751](https://www.pivotaltracker.com/story/show/176643751)

--- tasks/install-windows-updates/run.sh
+++ tasks/install-windows-updates/run.sh
@@ -7,9 +7,6 @@ $VCENTER_CA_CERT
 END_OF_CERT
 export GOVC_TLS_CA_CERTS=ca.crt
 
-WINDOWS_UPDATE_POWERSHELL_MODULE_URL='http://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/41459/25/PSWindowsUpdate.zip'
-wget $WINDOWS_UPDATE_POWERSHELL_MODULE_URL -O PSWindowsUpdate.zip
-
 export GOVC_URL="${CREDENTIAL_URL}"
 
 vm_ipath=${STEMBUILD_CONSTRUCT_TARGET_VM}
@@ -45,9 +42,9 @@ function run_pwsh_command_with_govc() {
 }
 
 wait_for_vm_to_come_up
-govc guest.upload -vm.ipath=${vm_ipath} -l=${vm_username}:${vm_password} PSWindowsUpdate.zip 'C:\PSWindowsUpdate.zip'
 
-run_pwsh_command_with_govc 'Expand-Archive -Path C:/PSWindowsUpdate.zip -DestinationPath C:/Windows/System32/WindowsPowerShell/v1.0/Modules'
+run_pwsh_command_with_govc 'Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force'
+run_pwsh_command_with_govc 'Install-Module -Name PSWindowsUpdate -MinimumVersion 2.1.0.1 -Force'
 
 returnWindowsUpdateCount="exit (([array](Get-WindowsUpdate)).Count)"
 echo "getting update count"
