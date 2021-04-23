3ea4f734e293a147460d648a71e9ec3ad7a00894
Joseph Palermo <jpalermo@pivotal.io>
2021-04-23 14:03:51 -0700

    Update contract test to allow us to use generic dns hostname for our nimbus environment

--- tasks/test-contract-stembuild-linux/run.sh
+++ tasks/test-contract-stembuild-linux/run.sh
@@ -8,6 +8,13 @@ echo "***Creating GOPATH environment & structure ***"
 export GOPATH=$PWD/gopath
 export PATH=${GOPATH}/bin:$PATH
 
+export VCENTER_CA_CERT="$(openssl s_client -showcerts -connect ${VCENTER_BASE_URL}:443 2>/dev/null </dev/null | \
+ sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p')"
+
+# We use an additional dns redirect that will cause TLS to fail
+# So we fetch the hostname we're supposed to be using from the Cert
+export VCENTER_BASE_URL=$(openssl x509 -noout -subject -in <(echo "$VCENTER_CA_CERT") | sed -e 's/^subject.*CN=\([a-zA-Z0-9\.\-]*\).*$/\1/')
+
 CF_INC_DIR=${GOPATH}/src/github.com/cloudfoundry-incubator
 STEMBUILD_DIR=${ROOT_DIR}/stembuild
 mkdir -p ${CF_INC_DIR}
