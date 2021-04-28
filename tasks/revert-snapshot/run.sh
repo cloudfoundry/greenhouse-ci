c1b5b1765211515aa1e450c85aef7ed9870fe730
Ruben Koster <rubenk@vmware.com>
2021-04-28 12:25:29 -0700

    Simplify usage of GOVC_URL
    [#177839999]
    
    Signed-off-by: Joseph Palermo <jpalermo@pivotal.io>
    Co-authored-by: Joseph Palermo <jpalermo@pivotal.io>

--- tasks/revert-snapshot/run.sh
+++ tasks/revert-snapshot/run.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 set -exu
 
-cat > ca.crt <<END_OF_CERT
-$VCENTER_CA_CERT
-END_OF_CERT
-export GOVC_TLS_CA_CERTS=ca.crt
+export GOVC_INSECURE=1
 
 echo "Retrieving device info to ensure vm has networking"
-if ! govc device.info  -u ${CREDENTIAL_URL} -vm.ipath=${VM_TO_REVERT} 'ethernet-*' ; then
+if ! govc device.info -vm.ipath=${VM_TO_REVERT} 'ethernet-*' ; then
     echo "No network device present, adding network device to VM"
-    govc vm.network.add  -u ${CREDENTIAL_URL} -vm.ipath=${VM_TO_REVERT} -net="internal-network" -net.adapter="e1000e"
-    govc device.info  -u ${CREDENTIAL_URL} -vm.ipath=${VM_TO_REVERT} 'ethernet-*'
+    govc vm.network.add -vm.ipath=${VM_TO_REVERT} -net="internal-network" -net.adapter="e1000e"
+    govc device.info -vm.ipath=${VM_TO_REVERT} 'ethernet-*'
 fi
 
 echo "Reverting ${VM_TO_REVERT} to snapshot ${SNAPSHOT_NAME}"
-govc snapshot.revert -u=${CREDENTIAL_URL} -s=true -dc=${DATACENTER} -vm=${VM_TO_REVERT} "${SNAPSHOT_NAME}"
-govc vm.power -u=${CREDENTIAL_URL} -dc=${DATACENTER} -on=true ${VM_TO_REVERT}
+govc snapshot.revert -s=true -dc=${DATACENTER} -vm=${VM_TO_REVERT} "${SNAPSHOT_NAME}"
+govc vm.power -dc=${DATACENTER} -on=true ${VM_TO_REVERT}
