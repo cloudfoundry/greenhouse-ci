809449c66c3c4c958ce4b00f145a05d98ab52ad9
aram price <pricear@vmware.com>
2023-09-22 15:15:06 -0700

    Stop moving `stembuild` into a pre-go.mod style path

--- tasks/test-units-stembuild-windows/run.ps1
+++ tasks/test-units-stembuild-windows/run.ps1
@@ -4,23 +4,8 @@ trap { Exit 1 }
 Import-Module ./ci/common-scripts/setup-windows-container.psm1
 Set-TmpDir
 
-$ROOT_DIR=Get-Location
-
-$GO_DIR=Join-Path $ROOT_DIR go-work
-$STEMBUILD_DIR="$GO_DIR/src/github.com/cloudfoundry/stembuild"
-
-$env:GOPATH = $GO_DIR
-Write-Host "GOPATH: $env:GOPATH"
-
-New-Item $GO_DIR -ItemType Directory
-
-Write-Host ***Cloning stembuild***
-Copy-Item stembuild $STEMBUILD_DIR -Recurse -Force
-
-$env:PATH="${GO_DIR}/bin;$env:PATH"
-
 Write-Host ***Test Stembuild Code***
-Set-Location $STEMBUILD_DIR
+Set-Location stembuild
 make units
 if ($lastexitcode -ne 0)
 {
