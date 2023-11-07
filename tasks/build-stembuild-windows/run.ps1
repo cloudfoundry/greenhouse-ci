3d1886b205f9820d51723fa286bd14762bb3bd60
aram price <pricear@vmware.com>
2023-11-07 15:21:20 -0800

    Stop moving `stembuild` into a pre-go.mod style path (.ps1 edition)

--- tasks/build-stembuild-windows/run.ps1
+++ tasks/build-stembuild-windows/run.ps1
@@ -8,30 +8,23 @@ $ROOT_DIR=Get-Location
 $OUTPUT_DIR=Join-Path $ROOT_DIR output
 $VERSION=Get-Content (Join-Path (Join-Path $ROOT_DIR version) version)
 
-$GO_DIR=Join-Path $ROOT_DIR go-work
-$STEMBUILD_DIR="$GO_DIR/src/github.com/cloudfoundry/stembuild"
-
 $env:GOPATH = $GO_DIR
 Write-Host "GOPATH: $env:GOPATH"
-
 New-Item $GO_DIR -ItemType Directory
-
-Write-Host ***Cloning stembuild***
-Copy-Item stembuild $STEMBUILD_DIR -Recurse -Force
-
 $env:PATH="${GO_DIR}/bin;$env:PATH"
+
 $env:PATH += ";c:\var\vcap\packages\git\usr\bin"
 
 Write-Host ***Building Stembuild***
-Set-Location $STEMBUILD_DIR
+Set-Location stembuild
 
-$STEMCELL_AUTOMATION_ZIP=Join-Path $ROOT_DIR $env:STEMCELL_AUTOMATION_ZIP
-$NEW_STEMCELL_AUTOMATION_ZIP=Join-Path $ROOT_DIR "StemcellAutomation.zip"
+$INPUT_ZIP_GLOB=Join-Path $ROOT_DIR $env:STEMCELL_AUTOMATION_ZIP
+$RENAMED_ZIP_PATH=Join-Path $ROOT_DIR "StemcellAutomation.zip"
 
-Move-Item -Path $STEMCELL_AUTOMATION_ZIP -Destination $NEW_STEMCELL_AUTOMATION_ZIP
+Copy-Item -Path $INPUT_ZIP_GLOB $NEW_STEMCELL_AUTOMATION_ZIP -Recurse -Force
 
 Write-Host "Using stemcell automation script: $NEW_STEMCELL_AUTOMATION_ZIP"
-make CGO_ENABLED=0 STEMCELL_VERSION=${VERSION} COMMAND=out/stembuild.exe AUTOMATION_PATH=${NEW_STEMCELL_AUTOMATION_ZIP} build
+make CGO_ENABLED=0 STEMCELL_VERSION=${VERSION} COMMAND=out/stembuild.exe AUTOMATION_PATH=${RENAMED_ZIP_PATH} build
 
 Write-Host ***Copying stembuild to output directory***
 Copy-Item out/stembuild.exe $OUTPUT_DIR/stembuild-windows-x86_64-$VERSION.exe
