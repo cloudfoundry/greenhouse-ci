9e78f81e35e98896a4210194804bb543c78cf90e
aram price <aram.price@broadcom.com>
2024-12-23 13:23:08 -0800

    CI: replace `package:agent` with bash

--- tasks/build-agent-zip/run.sh
+++ tasks/build-agent-zip/run.sh
@@ -2,23 +2,35 @@
 set -eu -o pipefail
 set -x
 
-BOSH_AGENT_DIR="$(pwd)/${BOSH_AGENT_DIR}"
+CONCOURSE_ROOT="$(pwd)"
+
+BOSH_AGENT_DIR="${CONCOURSE_ROOT}/${BOSH_AGENT_DIR}"
 export BOSH_AGENT_DIR
-pushd "${BOSH_AGENT_DIR}"
-  BOSH_AGENT_VERSION=$(cat .resource/version)
-  mv bosh-agent-pipe*.exe pipe.exe
-  mv bosh-agent*.exe bosh-agent.exe
-popd
 
-pushd stemcell-builder
-  pushd src/github.com/cloudfoundry/bosh-agent
-    git fetch
-    git checkout "v${BOSH_AGENT_VERSION}"
-  popd
+zip_dir="${CONCOURSE_ROOT}/zip-dir-root/"
+zip_deps_dir="${zip_dir}/deps/"
 
+mkdir -p "${zip_dir}"
+mkdir -p "${zip_deps_dir}"
+
+# agent-zip root
+cp "${BOSH_AGENT_DIR}"/bosh-agent-*.exe "${zip_dir}/bosh-agent.exe"
+cp "${BOSH_AGENT_DIR}"/git-sha "${zip_dir}/"
+cp "${BOSH_AGENT_DIR}"/service_wrapper.xml "${zip_dir}/service_wrapper.xml"
+
+cp "${CONCOURSE_ROOT}"/windows-winsw/WinSW.NET461.exe "${zip_dir}/service_wrapper.exe"
+
+# agent-zip deps
+cp "${BOSH_AGENT_DIR}"/bosh-agent-pipe-*.exe "${zip_deps_dir}/pipe.exe"
+
+cp "${CONCOURSE_ROOT}"/blobstore-s3-cli/s3cli-*.exe "${zip_deps_dir}/bosh-blobstore-s3.exe"
+cp "${CONCOURSE_ROOT}"/blobstore-gcs-cli/bosh-gcscli-*.exe "${zip_deps_dir}/bosh-blobstore-gcs.exe"
+cp "${CONCOURSE_ROOT}"/blobstore-dav-cli/davcli-*.exe "${zip_deps_dir}/bosh-blobstore-dav.exe"
+cp "${CONCOURSE_ROOT}"/windows-bsdtar/tar-*.exe "${zip_deps_dir}/tar.exe"
+cp "${CONCOURSE_ROOT}"/windows-winsw/WinSW.NET461.exe "${zip_deps_dir}/job-service-wrapper.exe"
+
+pushd "${zip_dir}"
   bundle install --without test
 
-  rake package:agent
-
-  mv build/*.zip ../bosh-agent
+  zip -r "${CONCOURSE_ROOT}/bosh-agent/agent.zip" ./*
 popd
