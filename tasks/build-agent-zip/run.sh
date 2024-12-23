3d28d17381e0b7fdaa7c90b51d59fed0cbe6db59
aram price <aram.price@broadcom.com>
2024-12-23 15:51:03 -0800

    CI: 🐞 prevent duplicate path segments

--- tasks/build-agent-zip/run.sh
+++ tasks/build-agent-zip/run.sh
@@ -4,8 +4,7 @@ set -x
 
 CONCOURSE_ROOT="$(pwd)"
 
-BOSH_AGENT_DIR="${CONCOURSE_ROOT}/${BOSH_AGENT_DIR}"
-export BOSH_AGENT_DIR
+bosh_agent_resource_dir="${CONCOURSE_ROOT}/${BOSH_AGENT_DIR}"
 
 zip_dir="${CONCOURSE_ROOT}/zip-dir-root/"
 zip_deps_dir="${zip_dir}/deps/"
@@ -13,17 +12,17 @@ zip_deps_dir="${zip_dir}/deps/"
 mkdir -p "${zip_dir}"
 mkdir -p "${zip_deps_dir}"
 
-bosh_agent_resource_version="$(cat "${BOSH_AGENT_DIR}/.resource/version")"
+bosh_agent_resource_version="$(cat "${bosh_agent_resource_dir}/.resource/version")"
 
 # agent-zip root
-cp "${BOSH_AGENT_DIR}/bosh-agent-${bosh_agent_resource_version}-windows-amd64.exe" "${zip_dir}/bosh-agent.exe"
-cp "${BOSH_AGENT_DIR}/git-sha" "${zip_dir}/"
-cp "${BOSH_AGENT_DIR}/service_wrapper.xml" "${zip_dir}/service_wrapper.xml"
+cp "${bosh_agent_resource_dir}/bosh-agent-${bosh_agent_resource_version}-windows-amd64.exe" "${zip_dir}/bosh-agent.exe"
+cp "${bosh_agent_resource_dir}/git-sha" "${zip_dir}/"
+cp "${bosh_agent_resource_dir}/service_wrapper.xml" "${zip_dir}/service_wrapper.xml"
 
 cp "${CONCOURSE_ROOT}"/windows-winsw/WinSW.NET461.exe "${zip_dir}/service_wrapper.exe"
 
 # agent-zip deps
-cp "${BOSH_AGENT_DIR}/bosh-agent-pipe-${bosh_agent_resource_version}-windows-amd64.exe" "${zip_deps_dir}/pipe.exe"
+cp "${bosh_agent_resource_dir}/bosh-agent-pipe-${bosh_agent_resource_version}-windows-amd64.exe" "${zip_deps_dir}/pipe.exe"
 
 cp "${CONCOURSE_ROOT}"/blobstore-gcs-cli/bosh-gcscli-*.exe "${zip_deps_dir}/bosh-blobstore-gcs.exe"
 cp "${CONCOURSE_ROOT}"/blobstore-dav-cli/davcli-*.exe "${zip_deps_dir}/bosh-blobstore-dav.exe"
