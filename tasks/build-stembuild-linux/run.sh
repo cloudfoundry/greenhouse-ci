daa2bbfc84b47217e394c0a71795f623dd1fea78
Brian Upton <brian.upton@broadcom.com>
2025-02-21 16:28:02 -0800

    Use standard Makefile for stembuild

--- tasks/build-stembuild-linux/run.sh
+++ tasks/build-stembuild-linux/run.sh
@@ -8,21 +8,12 @@ VERSION=$(cat version/version)
 STEMBUILD_DIR="${ROOT_DIR}/stembuild"
 OUTPUT_DIR="${ROOT_DIR}/output"
 
-INPUT_ZIP_GLOB="${ROOT_DIR}"/${STEMCELL_AUTOMATION_ZIP}
-# `old-Makefile` does not rename `AUTOMATION_PATH` so this filename must
-# match the expected go:empbed file in `stembuild/assets/stemcell_automation.go`
-RENAMED_ZIP_PATH="${ROOT_DIR}/StemcellAutomation.zip"
-cp ${INPUT_ZIP_GLOB} "${RENAMED_ZIP_PATH}"
-
-# Instead of retooling the job/pipeline, use a copy of the old makefile
-cp ci/tasks/build-stembuild-linux/old-Makefile  "${STEMBUILD_DIR}/Makefile"
+cp "${ROOT_DIR}"/${STEMCELL_AUTOMATION_ZIP} "${STEMBUILD_DIR}/assets/StemcellAutomation.zip"
 
 pushd "${STEMBUILD_DIR}"
   echo '***Building Stembuild***'
   make \
-    CGO_ENABLED=0 \
     STEMCELL_VERSION="${VERSION}" \
-    AUTOMATION_PATH="${RENAMED_ZIP_PATH}" \
     build
 popd
 
--- tasks/build-stembuild-windows/run.ps1
+++ tasks/build-stembuild-windows/run.ps1
@@ -3,9 +3,6 @@ Set-PSDebug -Trace 1
 $ErrorActionPreference = "Stop";
 trap { Exit 1 }
 
-# Instead of retooling the job/pipeline, use a copy of the old makefile
-Copy-Item ci/tasks/build-stembuild-windows/old-Makefile stembuild/Makefile -Recurse -Force
-
 $ROOT_DIR=Get-Location
 $OUTPUT_DIR=Join-Path $ROOT_DIR output
 $VERSION=Get-Content (Join-Path (Join-Path $ROOT_DIR version) version)
@@ -16,12 +13,10 @@ Write-Host ***Building Stembuild***
 Set-Location stembuild
 
 $INPUT_ZIP_GLOB=Join-Path $ROOT_DIR $env:STEMCELL_AUTOMATION_ZIP
-$RENAMED_ZIP_PATH=Join-Path $ROOT_DIR "StemcellAutomation.zip"
 
-Copy-Item -Path $INPUT_ZIP_GLOB $RENAMED_ZIP_PATH -Recurse -Force
+Copy-Item -Path $INPUT_ZIP_GLOB "assets\StemcellAutomation.zip"" -Recurse -Force
 
-Write-Host "Using stemcell automation script: $RENAMED_ZIP_PATH"
-make CGO_ENABLED=0 STEMCELL_VERSION=${VERSION} COMMAND=out/stembuild.exe AUTOMATION_PATH=${RENAMED_ZIP_PATH} build
+make STEMCELL_VERSION=${VERSION} build
 
 Write-Host ***Copying stembuild to output directory***
 Copy-Item out/stembuild.exe $OUTPUT_DIR/stembuild-windows-x86_64-$VERSION.exe
