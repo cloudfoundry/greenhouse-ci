379b9403eb692f32d0cea8d8aaa1b845ff80e4b7
Brian Upton <brian.upton@broadcom.com>
2025-02-21 16:32:18 -0800

    Use standard script for generating stembuild zip

--- tasks/zip-files/run.bash
+++ tasks/zip-files/run.bash
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
-cp open-ssh/OpenSSH-Win64.zip bosh-psmodules/bosh-psmodules.zip bosh-agent/agent.zip deps-file/deps.json stembuild/stemcell-automation
-zip -rj StemcellAutomation.zip stembuild/stemcell-automation -x \*.Tests.\* *NOTICE
-cp StemcellAutomation.zip "zip-file/StemcellAutomation-"`date +"%s"`.zip
+OPENSSH_ZIP=open-ssh/OpenSSH-Win64.zip \
+BOSH_PSMODULES_ZIP=bosh-psmodules/bosh-psmodules.zip \
+AGENT_ZIP=bosh-agent/agent.zip \
+DEPS_JSON=deps-file/deps.json \
+stembuild/bin/build-stemcell-automation-zip.sh
+
+cp stembuild/assets/StemcellAutomation.zip "zip-file/StemcellAutomation-"`date +"%s"`.zip
