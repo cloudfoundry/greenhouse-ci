50479bedf2294d2e5d04a4d67d364490c5d44003
aram price <aram.price@broadcom.com>
2025-05-07 10:51:35 -0700

    CI: use env var for govc auth / api target

--- ci/tasks/setup-integration-test-vm/run.sh
+++ ci/tasks/setup-integration-test-vm/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+export GOVC_URL="${VCENTER_ADMIN_CREDENTIAL_URL}"
+set -x
+
 ROOT_DIR=$(pwd)
 export OUTPUT_DIR=${ROOT_DIR}/output
 
@@ -17,20 +20,18 @@ export CLONE_NAME
 echo "${CLONE_NAME}" > integration-vm-name/name
 echo "Creating VM ${CLONE_NAME} with IP: ${VM_IP}"
 
-govc vm.clone \
-  -u "${VCENTER_ADMIN_CREDENTIAL_URL}" \
-  -vm "${BASE_VM_IPATH}" -ds "${CLONE_DATASTORE}" \
+govc vm.clone -verbose \
+  -vm "${BASE_VM_IPATH}" \
+  -ds "${CLONE_DATASTORE}" \
   -pool "${CLONE_RESOURCE_POOL}" \
   -folder "${CLONE_FOLDER}" \
   -on=false "${CLONE_NAME}"
 
-govc vm.customize \
-  -u "${VCENTER_ADMIN_CREDENTIAL_URL}" \
+govc vm.customize -verbose \
   -vm.ipath "${CLONE_FOLDER}"/"${CLONE_NAME}" \
   -ip "${VM_IP}" "${VM_CUSTOMIZATION_NAME}"
 
-govc vm.power -on \
-  -u "${VCENTER_ADMIN_CREDENTIAL_URL}" \
+govc vm.power -on -verbose \
   -vm.ipath "${CLONE_FOLDER}"/"${CLONE_NAME}"
 
 echo Waiting for VM to be configured with expected IP address...
@@ -39,7 +40,7 @@ FOUND_IP_ADDRESS=
 
 while [ "${VM_IP}" != "${FOUND_IP_ADDRESS}" ]; do
 	sleep 10
-	VM_INFO=$(govc vm.info -u "${VCENTER_ADMIN_CREDENTIAL_URL}" -json "${CLONE_FOLDER}"/"${CLONE_NAME}")
+	VM_INFO=$(govc vm.info -json "${CLONE_FOLDER}"/"${CLONE_NAME}")
 
 	FOUND_IP_ADDRESS=$(echo "${VM_INFO}" |
 	    jq -r ".VirtualMachines[0].Guest.Net[0].IpAddress | .[]? |select(. == \"${VM_IP}\")")
