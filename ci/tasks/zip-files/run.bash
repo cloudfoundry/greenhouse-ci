7b983612aed99c2e94abe4c7bf909356a8d46c08
Brian Upton <brian.upton@broadcom.com>
2025-05-07 13:06:52 -0700

    Use correctly nested stembuild dir
    Signed-off-by: Aram Price <aram.price@broadcom.com>

--- ci/tasks/zip-files/run.bash
+++ ci/tasks/zip-files/run.bash
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+ROOT_DIR=$(pwd)
+STEMBUILD_DIR="${ROOT_DIR}/stembuild/stembuild"
+
 OPENSSH_ZIP=open-ssh/OpenSSH-Win64.zip \
 BOSH_PSMODULES_ZIP=bosh-psmodules/bosh-psmodules.zip \
 AGENT_ZIP=bosh-agent/agent.zip \
 DEPS_JSON=deps-file/deps.json \
-stembuild/bin/build-stemcell-automation-zip.sh
+"${STEMBUILD_DIR}/build-stemcell-automation-zip.sh"
 
-cp stembuild/assets/StemcellAutomation.zip "zip-file/StemcellAutomation-"`date +"%s"`.zip
+cp "${STEMBUILD_DIR}/assets/StemcellAutomation.zip" "zip-file/StemcellAutomation-$(date +"%s").zip"
