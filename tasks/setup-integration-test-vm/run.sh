9806c4684157dff9306747199fcdaf446b78126c
Brian Upton <brian.upton@broadcom.com>
2025-05-07 11:38:45 -0700

    Add verbose flag to govc commands
    Signed-off-by: Aram Price <aram.price@broadcom.com>

--- tasks/setup-integration-test-vm/run.sh
+++ tasks/setup-integration-test-vm/run.sh
@@ -2,6 +2,10 @@
 
 set -eu
 
+export GOVC_URL="${VCENTER_ADMIN_CREDENTIAL_URL}"
+
+set -x
+
 export CLONE_NAME_PREFIX="construct-${JOB_OS_NAME}-integration-ci-${OS_LINE}"
 
 ROOT_DIR=$(pwd)
@@ -13,16 +17,16 @@ export CLONE_NAME=${CLONE_NAME_PREFIX}${CLONE_NAME_SUFFIX}
 echo ${CLONE_NAME} > integration-vm-name/name
 echo "Creating VM ${CLONE_NAME} with IP: ${VM_IP}"
 
-govc vm.clone -u ${VCENTER_ADMIN_CREDENTIAL_URL} -vm ${BASE_VM_IPATH} -ds ${CLONE_DATASTORE} -pool ${CLONE_RESOURCE_POOL} -folder "$CLONE_FOLDER" -on=false "$CLONE_NAME"
-govc vm.customize -u ${VCENTER_ADMIN_CREDENTIAL_URL} -vm.ipath "$CLONE_FOLDER"/"$CLONE_NAME" -ip ${VM_IP} ${VM_CUSTOMIZATION_NAME}
-govc vm.power -on -u ${VCENTER_ADMIN_CREDENTIAL_URL} -vm.ipath "$CLONE_FOLDER"/"$CLONE_NAME"
+govc vm.clone -verbose -vm ${BASE_VM_IPATH} -ds ${CLONE_DATASTORE} -pool ${CLONE_RESOURCE_POOL} -folder "$CLONE_FOLDER" -on=false "$CLONE_NAME"
+govc vm.customize -verbose -vm.ipath "$CLONE_FOLDER"/"$CLONE_NAME" -ip ${VM_IP} ${VM_CUSTOMIZATION_NAME}
+govc vm.power -verbose -on -vm.ipath "$CLONE_FOLDER"/"$CLONE_NAME"
 
 echo Waiting for VM to be configured with expected IP address...
 SECONDS=0
 FOUND_IP_ADDRESS=
 while [ "$VM_IP" != "$FOUND_IP_ADDRESS" ]; do
 	sleep 10
-	VM_INFO=$(govc vm.info -u "$VCENTER_ADMIN_CREDENTIAL_URL" -json "$CLONE_FOLDER"/"$CLONE_NAME")
+	VM_INFO=$(govc vm.info -json "$CLONE_FOLDER"/"$CLONE_NAME")
 
 	FOUND_IP_ADDRESS=$(echo $VM_INFO |
 	    jq -r ".VirtualMachines[0].Guest.Net[0].IpAddress | .[]? |select(. == \"$VM_IP\")")
