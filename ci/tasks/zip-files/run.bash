3c17bcff058f6d9effa9c817eff190e838ebdbe2
Aram Price <aram.price@broadcom.com>
2025-05-07 14:13:00 -0700

    Fix build-stemcell-automation-zip path
    Signed-off-by: Brian Upton <brian.upton@broadcom.com>

--- ci/tasks/zip-files/run.bash
+++ ci/tasks/zip-files/run.bash
@@ -8,6 +8,6 @@ OPENSSH_ZIP=open-ssh/OpenSSH-Win64.zip \
 BOSH_PSMODULES_ZIP=bosh-psmodules/bosh-psmodules.zip \
 AGENT_ZIP=bosh-agent/agent.zip \
 DEPS_JSON=deps-file/deps.json \
-"${STEMBUILD_DIR}/build-stemcell-automation-zip.sh"
+"${ROOT_DIR}/stembuild/bin/build-stemcell-automation-zip.sh"
 
 cp "${STEMBUILD_DIR}/assets/StemcellAutomation.zip" "zip-file/StemcellAutomation-$(date +"%s").zip"
